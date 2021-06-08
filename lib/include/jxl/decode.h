a98778e7f109a7b3b037210bbbdbf906cc0877b2
Lode <lode@google.com>
2021-06-08 13:45:46 +0200

    mark DC API functions as deprecated

--- lib/include/jxl/decode.h
+++ lib/include/jxl/decode.h
@@ -150,6 +150,8 @@ typedef enum {
    * requested and it is possible to decode a DC image from the codestream and
    * the DC out buffer was not yet set. This event re-occurs for new frames
    * if there are multiple animation frames.
+   * DEPRECATED: the DC feature in this form will be removed. You can use
+   * JxlDecoderFlushImage for progressive rendering.
    */
   JXL_DEC_NEED_DC_OUT_BUFFER = 4,
 
@@ -214,6 +216,8 @@ typedef enum {
    * status only indicates we're past this point in the codestream. This event
    * occurs max once per frame and always later than JXL_DEC_FRAME_HEADER
    * and other header events and earlier than full resolution pixel data.
+   * DEPRECATED: the DC feature in this form will be removed. You can use
+   * JxlDecoderFlushImage for progressive rendering.
    */
   JXL_DEC_DC_IMAGE = 0x800,
 
@@ -693,8 +697,11 @@ JXL_EXPORT JxlDecoderStatus JxlDecoderGetFrameName(const JxlDecoder* dec,
  * @param size output value, buffer size in bytes
  * @return JXL_DEC_SUCCESS on success, JXL_DEC_ERROR on error, such as
  *    information not available yet.
+ *
+ * DEPRECATED: the DC feature in this form will be removed. You can use
+ * JxlDecoderFlushImage for progressive rendering.
  */
-JXL_EXPORT JxlDecoderStatus JxlDecoderDCOutBufferSize(
+JXL_EXPORT JXL_DEPRECATED JxlDecoderStatus JxlDecoderDCOutBufferSize(
     const JxlDecoder* dec, const JxlPixelFormat* format, size_t* size);
 
 /**
@@ -711,8 +718,11 @@ JXL_EXPORT JxlDecoderStatus JxlDecoderDCOutBufferSize(
  * @param size size of buffer in bytes
  * @return JXL_DEC_SUCCESS on success, JXL_DEC_ERROR on error, such as
  * size too small.
+ *
+ * DEPRECATED: the DC feature in this form will be removed. You can use
+ * JxlDecoderFlushImage for progressive rendering.
  */
-JXL_EXPORT JxlDecoderStatus JxlDecoderSetDCOutBuffer(
+JXL_EXPORT JXL_DEPRECATED JxlDecoderStatus JxlDecoderSetDCOutBuffer(
     JxlDecoder* dec, const JxlPixelFormat* format, void* buffer, size_t size);
 
 /**
