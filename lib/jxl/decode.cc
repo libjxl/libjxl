00367083c2641a10a17179ba1fcbda7079fbe3c1
Lode <lode@google.com>
2021-06-17 21:18:01 +0200

    Verify JxlPixelFormat validity when setting out callback
    The format must be checked for having a supported amount of channels,
    pixel type, etc... This was done in the size check for the image output
    buffer setters, but was not done in the callback because buffer size
    works differently for those.
    
    Now the full check is done here as well to fix that.

--- lib/jxl/decode.cc
+++ lib/jxl/decode.cc
@@ -2111,6 +2111,11 @@ JxlDecoderStatus JxlDecoderSetImageOutCallback(JxlDecoder* dec,
         "Cannot change from image out buffer to image out callback");
   }
 
+  // Perform error checking for invalid format.
+  size_t bits_dummy;
+  JxlDecoderStatus status = PrepareSizeCheck(dec, format, &bits_dummy);
+  if (status != JXL_DEC_SUCCESS) return status;
+
   dec->image_out_buffer_set = true;
   dec->image_out_callback = callback;
   dec->image_out_opaque = opaque;
