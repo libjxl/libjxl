89889a1fb5970434a51f41bfaa06fe2553617ca9
Luca Versari <veluca@google.com>
2021-07-12 13:28:32 +0200

    SIMD-fy noise generation.
    This gets rid of a significant fraction of noise's cost on the decoder
    side. The remaining part is tied to the noise buffers, which are
    image-sized but need not be.
    
    ~10^-9 variations in output, likely due to different order of
    operations.
    
    ```
    Before:
    Compr              Input    Compr            Compr       Compr  Decomp  Butteraugli
    Method            Pixels     Size              BPP   #    MP/s    MP/s     Distance    Error p norm           BPP*pnorm   Errors
    --------------------------------------------------------------------------------------------------------------------------------
    jxl:d0.7        13270449  3376300    2.03537951127   1   5.565  26.594   3.97672892   0.81331137439  1.6553973077103630        0
    jxl:d1          13270449  2668722    1.60882092234   1   5.854  29.188   1.86357200   0.63033095952  1.0140896356701443        0
    jxl:d2          13270449  1694049    1.02124592770   1   6.165  28.236   3.99444389   1.10166614051  1.1250720596782238        0
    jxl:d4          13270449  1023226    0.61684484074   1   6.020  23.051   4.65989637   1.57546758763  0.9718190531867815        0
    jxl:d8          13270449   583296    0.35163602980   1   5.845  22.576   8.88710022   2.47537073649  0.8704295380596414        0
    Aggregate:      13270449  1555630    0.93780069287 ---   5.886  25.789   4.14660785   1.17107589664  1.0982357872766344        0
    
    After:
    Compr              Input    Compr            Compr       Compr  Decomp  Butteraugli
    Method            Pixels     Size              BPP   #    MP/s    MP/s     Distance    Error p norm           BPP*pnorm   Errors
    --------------------------------------------------------------------------------------------------------------------------------
    jxl:d0.7        13270449  3376300    2.03537951127   1   5.470  37.234   3.97672892   0.81331137374  1.6553973063866161        0
    jxl:d1          13270449  2668722    1.60882092234   1   5.760  39.695   1.86357200   0.63033096010  1.0140896366012817        0
    jxl:d2          13270449  1694049    1.02124592770   1   6.067  40.498   3.99444437   1.10166613996  1.1250720591163290        0
    jxl:d4          13270449  1023226    0.61684484074   1   5.912  30.427   4.65989637   1.57546758886  0.9718190539455960        0
    jxl:d8          13270449   583296    0.35163602980   1   5.746  29.539   8.88710022   2.47537074204  0.8704295400081206        0
    Aggregate:      13270449  1555630    0.93780069287 ---   5.787  35.168   4.14660795   1.17107589726  1.0982357878561646        0
    
    Without noise:
    Compr              Input    Compr            Compr       Compr  Decomp  Butteraugli
    Method            Pixels     Size              BPP   #    MP/s    MP/s     Distance    Error p norm           BPP*pnorm   Errors
    --------------------------------------------------------------------------------------------------------------------------------
    jxl:d0.7        13270449  3376020    2.03521071518   1   5.833  47.597   1.48137546   0.48043308217  0.9777825567553876        0
    jxl:d1          13270449  2668442    1.60865212624   1   6.179  51.007   1.77268112   0.61341852300  0.9867770112915698        0
    jxl:d2          13270449  1693685    1.02102649277   1   6.533  53.021   2.93441439   0.96843094388  0.9887936501245655        0
    jxl:d4          13270449  1022946    0.61667604465   1   6.380  37.185   4.81405544   1.56954856488  0.9679030008724722        0
    jxl:d8          13270449   583016    0.35146723370   1   6.175  35.851   9.03337669   2.58973218211  0.9102060060710633        0
    Aggregate:      13270449  1555270    0.93758378970 ---   6.216  44.351   3.19915795   1.03014322850  0.9658455921082402        0
    ```

--- lib/jxl/dec_noise.cc
+++ lib/jxl/dec_noise.cc
@@ -31,7 +31,9 @@ namespace HWY_NAMESPACE {
 using hwy::HWY_NAMESPACE::ShiftRight;
 using hwy::HWY_NAMESPACE::Vec;
 
-using D = HWY_CAPPED(float, 1);
+using D = HWY_CAPPED(float, kBlockDim);
+using DI = hwy::HWY_NAMESPACE::Rebind<int, D>;
+using DI8 = hwy::HWY_NAMESPACE::Repartition<uint8_t, D>;
 
 // Converts one vector's worth of random bits to floats in [1, 2).
 // NOTE: as the convolution kernel sums to 0, it doesn't matter if inputs are in
@@ -103,21 +105,68 @@ class StrengthEvalLut {
   using V = Vec<D>;
 
   explicit StrengthEvalLut(const NoiseParams& noise_params)
-      : noise_params_(noise_params) {}
+#if HWY_TARGET == HWY_SCALAR
+      : noise_params_(noise_params)
+#endif
+  {
+#if HWY_TARGET != HWY_SCALAR
+    uint32_t lut[8];
+    memcpy(lut, noise_params.lut, sizeof(lut));
+    for (size_t i = 0; i < 8; i++) {
+      low16_lut[2 * i] = (lut[i] >> 0) & 0xFF;
+      low16_lut[2 * i + 1] = (lut[i] >> 8) & 0xFF;
+      high16_lut[2 * i] = (lut[i] >> 16) & 0xFF;
+      high16_lut[2 * i + 1] = (lut[i] >> 24) & 0xFF;
+    }
+#endif
+  }
 
   V operator()(const V vx) const {
-    float x;
-    Store(vx, D(), &x);
-    std::pair<int, float> pos = IndexAndFrac(x);
-    JXL_DASSERT(pos.first >= 0 && static_cast<size_t>(pos.first) <
-                                      NoiseParams::kNumNoisePoints - 1);
-    float low = noise_params_.lut[pos.first];
-    float hi = noise_params_.lut[pos.first + 1];
-    return Set(D(), low * (1.0f - pos.second) + hi * pos.second);
+    constexpr size_t kScale = NoiseParams::kNumNoisePoints - 2;
+    auto scaled_vx = Max(Zero(D()), vx * Set(D(), kScale));
+    auto floor_x = Floor(scaled_vx);
+    auto frac_x = scaled_vx - floor_x;
+    floor_x = IfThenElse(scaled_vx >= Set(D(), kScale), Set(D(), kScale - 1),
+                         floor_x);
+    frac_x = IfThenElse(scaled_vx >= Set(D(), kScale), Set(D(), 1), frac_x);
+    auto floor_x_int = ConvertTo(DI(), floor_x);
+#if HWY_TARGET == HWY_SCALAR
+    auto low = Set(D(), noise_params_.lut[floor_x_int.raw]);
+    auto hi = Set(D(), noise_params_.lut[floor_x_int.raw + 1]);
+#else
+    // Set each lane's bytes to {0, 0, 2x+1, 2x}.
+    auto floorx_indices_low =
+        floor_x_int * Set(DI(), 0x0202) + Set(DI(), 0x0100);
+    // Set each lane's bytes to {2x+1, 2x, 0, 0}.
+    auto floorx_indices_hi =
+        floor_x_int * Set(DI(), 0x02020000) + Set(DI(), 0x01000000);
+    // load LUT
+    auto low16 = BitCast(DI(), LoadDup128(DI8(), low16_lut));
+    auto lowm = Set(DI(), 0xFFFF);
+    auto hi16 = BitCast(DI(), LoadDup128(DI8(), high16_lut));
+    auto him = Set(DI(), 0xFFFF0000);
+    // low = noise_params.lut[floor_x]
+    auto low =
+        BitCast(D(), (TableLookupBytes(low16, floorx_indices_low) & lowm) |
+                         (TableLookupBytes(hi16, floorx_indices_hi) & him));
+    // hi = noise_params.lut[floor_x+1]
+    floorx_indices_low += Set(DI(), 0x0202);
+    floorx_indices_hi += Set(DI(), 0x02020000);
+    auto hi =
+        BitCast(D(), (TableLookupBytes(low16, floorx_indices_low) & lowm) |
+                         (TableLookupBytes(hi16, floorx_indices_hi) & him));
+#endif
+    return MulAdd(hi - low, frac_x, low);
   }
 
  private:
-  const NoiseParams noise_params_;
+#if HWY_TARGET != HWY_SCALAR
+  // noise_params.lut transformed into two 16-bit lookup tables.
+  HWY_ALIGN uint8_t high16_lut[16];
+  HWY_ALIGN uint8_t low16_lut[16];
+#else
+  const NoiseParams& noise_params_;
+#endif
 };
 
 template <class D>
