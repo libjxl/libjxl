1bb4fb4b062837a3158925f4ed70ee778b548620
Lode <lode@google.com>
2021-09-03 14:40:45 +0200

    Header only: box API design
    This MR proposes API for getting metadata boxes in the decoder.
    
    It is integrated with the event based system the decoder uses,
    using the JXL_DEC_BOX event to indicate a new box.
    
    Boxes are output to a byte buffer owned by the user, similar to the
    recompressed JPEG output.
    
    Boxes can be gotten in an uncompressed mode or in a raw mode. For
    non-brob boxes, these modes are the same. For brob boxes, the standard
    mode automaticlaly decompresses them, the raw mode returns them as-is.

--- lib/include/jxl/types.h
+++ lib/include/jxl/types.h
@@ -109,6 +109,10 @@ typedef struct {
   size_t align;
 } JxlPixelFormat;
 
+/** Data type holding the 4-character type name of an ISOBMFF box.
+ */
+typedef char JxlBoxType[4];
+
 #if defined(__cplusplus) || defined(c_plusplus)
 }
 #endif
