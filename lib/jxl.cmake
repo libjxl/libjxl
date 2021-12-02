4d472c6034f38d26b2b1d2eff635907417eecffd
Luca Versari <veluca@google.com>
2021-12-02 22:02:10 +0100

    Add a "Write to IB" stage.

--- lib/jxl.cmake
+++ lib/jxl.cmake
@@ -196,6 +196,8 @@ set(JPEGXL_INTERNAL_SOURCES_DEC
   jxl/render_pipeline/render_pipeline_stage.h
   jxl/render_pipeline/simple_render_pipeline.cc
   jxl/render_pipeline/simple_render_pipeline.h
+  jxl/render_pipeline/stage_write_to_ib.cc
+  jxl/render_pipeline/stage_write_to_ib.h
   jxl/render_pipeline/test_render_pipeline_stages.h
   jxl/sanitizers.h
   jxl/splines.cc
