9eb98cbfecb1534fde0e6fdabb0386226ae384e0
Thomas Fischbacher <tfish@google.com>
2021-12-08 16:41:05 +0100

    cjxl_ng: Adding some skeletal flags, and TODOs for to-add flags.

--- tools/cjxl_ng_main.cc
+++ tools/cjxl_ng_main.cc
@@ -30,6 +30,85 @@ ABSL_FLAG(bool, strip, false,
           "Do not encode using container format (strips "
           "Exif/XMP/JPEG bitstream reconstruction data)");
 
+ABSL_FLAG(bool, progressive, false,
+          "Enable progressive/responsive decoding.");
+
+ABSL_FLAG(bool, progressive_ac, false,
+          "Use progressive mode for AC.");
+
+ABSL_FLAG(bool, qprogressive_ac, false,
+          // TODO(tfish): Clarify what this flag is about.
+          "Use progressive mode for AC.");
+
+ABSL_FLAG(bool, progressive_dc, false,
+          "Use progressive mode for DC.");
+
+ABSL_FLAG(bool, modular, false,
+          "Use modular mode (lossy / lossless).");
+
+ABSL_FLAG(bool, use_experimental_encoder_heuristics, false,
+          "Use new and not yet ready encoder heuristics");
+
+ABSL_FLAG(bool, jpeg_transcode, false,
+          "Do lossy transcode of input JPEG file (decode to "
+          "pixels instead of doing lossless transcode).");
+
+ABSL_FLAG(bool, jpeg_transcode_disable_cfl, false,
+          "Disable CFL for lossless JPEG recompression");
+
+ABSL_FLAG(bool, premultiply, false,
+          "Force premultiplied (associated) alpha.");
+
+ABSL_FLAG(bool, keep_invisible, false,
+          "force disable/enable preserving color of invisible "
+          "pixels..");
+
+ABSL_FLAG(bool, centerfirst, false,
+          "Put center groups first in the compressed file.");
+
+ABSL_FLAG(bool, noise, false,
+          "force disable/enable noise generation.");
+
+ABSL_FLAG(bool, verbose, false,
+          // TODO(tfish): Should be a verbosity-level.
+          // Original cjxl also makes --help more verbose if this is on,
+          // but with absl, we do that differently...?
+          "Verbose output.");
+
+
+ABSL_FLAG(std::string, photon_noise, "ISO3200",
+          "Set the noise to approximately what it would be at a given nominal "
+          "exposure on a 35mm camera. For formats other than 35mm, or when the "
+          "whole sensor was not used, you can multiply the ISO value by the "
+          "equivalence ratio squared, for example by 2.25 for an APS-C "
+          "camera.");
+
+// TODO(tfish): --dots, --patches, --resampling, --ec_resampling,
+// --already_downsampled, --epf, --gaborish, --intensity_target,
+// --saliency_num_progressive_steps, --saliency_map_filename,
+// --saliency_threshold, --dec-hints, --override_bitdepth,
+// --colortransform, --mquality, --iterations, --colorspace, --group-size,
+// --predictor, --extra-properties, --lossy-palette, --pre-compact,
+// --post-compact, --responsive, --version, --quiet, --print_profile,
+
+ABSL_FLAG(int64_t, center_x, 0,
+          // TODO(tfish): Clarify if this is really the comment we want here.
+          "Put center groups first in the compressed file.");
+
+ABSL_FLAG(int64_t, center_y, 0,
+          // TODO(tfish): Clarify if this is really the comment we want here.
+          "Put center groups first in the compressed file.");
+
+ABSL_FLAG(int64_t, num_threads, 0,
+          "number of worker threads (zero = none).");
+
+ABSL_FLAG(int64_t, num_reps, 1,
+          // TODO(tfish): Clarify meaning of this docstring.
+          // Is this simply for benchmarking?
+          "how many times to compress.");
+
+
+
 ABSL_FLAG(float, distance, 1.0,
           "Max. butteraugli distance, lower = higher quality. Range: 0 .. 25.\n"
           "    0.0 = mathematically lossless. Default for already-lossy input "
@@ -37,14 +116,27 @@ ABSL_FLAG(float, distance, 1.0,
           "    1.0 = visually lossless. Default for other input.\n"
           "    Recommended range: 0.5 .. 3.0.");
 
-ABSL_FLAG(int64_t, size, 0,
+ABSL_FLAG(int64_t, target_size, 0,
           "Aim at file size of N bytes.\n"
           "    Compresses to 1 % of the target size in ideal conditions.\n"
           "    Runs the same algorithm as --target_bpp");
 
-ABSL_FLAG(int64_t, target_bpp, 0,
+ABSL_FLAG(float, target_bpp, 0,
           "Aim at file size that has N bits per pixel.\n"
           "    Compresses to 1 % of the target BPP in ideal conditions.");
+
+ABSL_FLAG(float, quality, 100.0,
+          "Quality setting (is remapped to --distance). Range: -inf .. 100.\n"
+          "    100 = mathematically lossless. Default for already-lossy input "
+          "(JPEG/GIF).\n    Positive quality values roughly match libjpeg "
+          "quality.");
+
+ABSL_FLAG(int64_t, effort, 7,
+          "Encoder effort setting. Range: 1 .. 9.\n"
+          "    Default: 7. Higher number is more effort (slower).");
+
+// TODO(tfish): --speed, --faster_decoding
+
 
 
 namespace {
