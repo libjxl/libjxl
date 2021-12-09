5ba7d6b0dd1c2d9e25d1cb9932c1fba8c652ca40
Luca Versari <veluca@google.com>
2021-12-09 19:23:13 +0100

    Add 2-, 4-, and 8- way interleaves.

--- lib/jxl.cmake
+++ lib/jxl.cmake
@@ -206,6 +206,7 @@ set(JPEGXL_INTERNAL_SOURCES_DEC
   jxl/render_pipeline/stage_xyb.h
   jxl/render_pipeline/test_render_pipeline_stages.h
   jxl/sanitizers.h
+  jxl/simd_util-inl.h
   jxl/splines.cc
   jxl/splines.h
   jxl/toc.cc
