8c9e3bd038eb3aefec24feba0704ff9e22c73a3a
Luca Versari <veluca@google.com>
2021-12-13 15:17:00 +0100

    Chroma upsampling stage

--- lib/jxl.cmake
+++ lib/jxl.cmake
@@ -196,6 +196,8 @@ set(JPEGXL_INTERNAL_SOURCES_DEC
   jxl/render_pipeline/render_pipeline_stage.h
   jxl/render_pipeline/simple_render_pipeline.cc
   jxl/render_pipeline/simple_render_pipeline.h
+  jxl/render_pipeline/stage_chroma_upsampling.cc
+  jxl/render_pipeline/stage_chroma_upsampling.h
   jxl/render_pipeline/stage_epf.cc
   jxl/render_pipeline/stage_epf.h
   jxl/render_pipeline/stage_gaborish.cc
