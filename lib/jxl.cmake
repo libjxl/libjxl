2bc1c1925cbb337b8965d1116e9e023207afcffe
Luca Versari <veluca@google.com>
2021-12-13 15:27:16 +0100

    Add a YCbCr stage.

--- lib/jxl.cmake
+++ lib/jxl.cmake
@@ -206,6 +206,8 @@ set(JPEGXL_INTERNAL_SOURCES_DEC
   jxl/render_pipeline/stage_write_to_ib.h
   jxl/render_pipeline/stage_xyb.cc
   jxl/render_pipeline/stage_xyb.h
+  jxl/render_pipeline/stage_ycbcr.cc
+  jxl/render_pipeline/stage_ycbcr.h
   jxl/render_pipeline/test_render_pipeline_stages.h
   jxl/sanitizers.h
   jxl/simd_util-inl.h
