ece5328d3e93233ff5c20a207117603ea5d51f5d
Jon Sneyers <jon@cloudinary.com>
2021-12-23 17:03:42 +0100

    fix non-coalesced frame dimension in case of upsampling

--- lib/jxl/decode.cc
+++ lib/jxl/decode.cc
@@ -850,8 +850,8 @@ void GetCurrentDimensions(const JxlDecoder* dec, size_t& xsize, size_t& ysize,
   xsize = dec->metadata.oriented_xsize(dec->keep_orientation || !oriented);
   ysize = dec->metadata.oriented_ysize(dec->keep_orientation || !oriented);
   if (!dec->coalescing) {
-    xsize = dec->frame_header->ToFrameDimensions().xsize;
-    ysize = dec->frame_header->ToFrameDimensions().ysize;
+    xsize = dec->frame_header->ToFrameDimensions().xsize_upsampled;
+    ysize = dec->frame_header->ToFrameDimensions().ysize_upsampled;
     if (!dec->keep_orientation && oriented &&
         static_cast<int>(dec->metadata.m.GetOrientation()) > 4) {
       std::swap(xsize, ysize);
