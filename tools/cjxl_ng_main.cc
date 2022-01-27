7c0e4aaa109beb8a22d591e1467840636ead1dd3
Thomas Fischbacher <tfish@google.com>
2022-01-27 14:32:25 +0100

    cjxl_ng: Adding more flags.

--- tools/cjxl_ng_main.cc
+++ tools/cjxl_ng_main.cc
@@ -52,7 +52,7 @@ DEFINE_bool(strip, false,
             "Do not encode using container format (strips "
             "Exif/XMP/JPEG bitstream reconstruction data).");
 
-DEFINE_bool(responsive, false, "[modular encoding] do Squeeze transform");
+DEFINE_bool(responsive, false, "[modular encoding] Do Squeeze transform");
 
 DEFINE_bool(progressive, false, "Enable progressive/responsive decoding.");
 
@@ -192,11 +192,34 @@ DEFINE_int32(modular_colorspace, -1,
              "[modular encoding] color transform: 0=RGB, 1=YCoCg, "
              "2-37=RCT (default: try several, depending on speed)");
 
+DEFINE_int32(
+    modular_channel_colors_global_percent, -1,
+    "[modular encoding] Use Global channel palette if the number of "
+    "colors is smaller than this percentage of range. "
+    "Use 0-100 to set an explicit percentage, -1 to use the encoder default.");
+
+DEFINE_int32(
+    modular_channel_colors_group_percent, -1,
+    "[modular encoding] Use Local (per-group) channel palette if the number "
+    "of colors is smaller than this percentage of range. Use 0-100 to set "
+    "an explicit percentage, -1 to use the encoder default.");
+
+DEFINE_int32(
+    modular_palette_colors, -1,
+    "[modular encoding] Use color palette if number of colors is smaller "
+    "than or equal to this, or -1 to use the encoder default.");
+
 DEFINE_int32(modular_nb_prev_channels, -1,
              // TODO(tfish): Clarify renaming (from --extra-properties),
              // as for --modular_group_size. Is this actually the
              // correct parameter?
              "[modular encoding] number of extra MA tree properties to use");
+
+DEFINE_int32(modular_ma_tree_learning_percent, -1,
+             "[modular encoding] Fraction of pixels used to learn MA trees as "
+             "a percentage. -1 = default, 0 = no MA and fast decode, 50 = "
+             "default value, 100 = all, values above 100 are also permitted. "
+             "Higher values use more encoder memory.");
 
 DEFINE_int32(photon_noise, 0,
              // TODO(tfish): Discuss docstring change with team.
@@ -240,6 +263,11 @@ DEFINE_int64(
     // but enc_params.h has kFalcon=7.
     "Encoder effort setting. Range: 1 .. 9.\n"
     "    Default: 7. Higher number is more effort (slower).");
+
+DEFINE_string(frame_indexing, "",
+              "If non-empty, a string matching '^[01]*'. If this string has a "
+              "'1' in i-th position, then the i-th frame will be indexed in "
+              "the frame index box.");
 
 DEFINE_string(
     // TODO(tfish): Clarify with team whether changing from int-param to string
@@ -573,11 +601,25 @@ int main(int argc, char** argv) {
         !gflags::GetCommandLineFlagInfoOrDie("modular_predictor").is_default;
     bool modular_colorspace_set =
         !gflags::GetCommandLineFlagInfoOrDie("modular_colorspace").is_default;
+    bool modular_ma_tree_learning_percent_set =
+        !gflags::GetCommandLineFlagInfoOrDie("modular_ma_tree_learning_percent")
+             .is_default;
     bool modular_nb_prev_channels_set =
         !gflags::GetCommandLineFlagInfoOrDie("modular_nb_prev_channels")
              .is_default;
     bool modular_lossy_palette_set =
         !gflags::GetCommandLineFlagInfoOrDie("modular_lossy_palette")
+             .is_default;
+    bool modular_palette_colors_set =
+        !gflags::GetCommandLineFlagInfoOrDie("modular_palette_colors")
+             .is_default;
+    bool modular_channel_colors_global_percent_set =
+        !gflags::GetCommandLineFlagInfoOrDie(
+             "modular_channel_colors_global_percent")
+             .is_default;
+    bool modular_channel_colors_group_percent_set =
+        !gflags::GetCommandLineFlagInfoOrDie(
+             "modular_channel_colors_group_percent")
              .is_default;
 
     if (modular_group_size_set) {
@@ -611,6 +653,18 @@ int main(int argc, char** argv) {
           jxl_encoder_frame_settings, JXL_ENC_FRAME_SETTING_MODULAR_COLOR_SPACE,
           FLAGS_modular_colorspace);
     }
+    if (modular_ma_tree_learning_percent_set) {
+      if (!(-1 <= FLAGS_modular_ma_tree_learning_percent &&
+            FLAGS_modular_ma_tree_learning_percent <= 100)) {
+        std::cerr << "Invalid --modular_ma_tree_learning_percent: "
+                  << FLAGS_modular_ma_tree_learning_percent << std::endl;
+        return EXIT_FAILURE;
+      }
+      JxlEncoderFrameSettingsSetOption(
+          jxl_encoder_frame_settings,
+          JXL_ENC_FRAME_SETTING_MODULAR_MA_TREE_LEARNING_PERCENT,
+          FLAGS_modular_ma_tree_learning_percent);
+    }
     if (modular_nb_prev_channels_set) {
       if (!(-1 <= FLAGS_modular_nb_prev_channels &&
             FLAGS_modular_nb_prev_channels <= 11)) {
@@ -627,6 +681,35 @@ int main(int argc, char** argv) {
       JxlEncoderFrameSettingsSetOption(jxl_encoder_frame_settings,
                                        JXL_ENC_FRAME_SETTING_LOSSY_PALETTE,
                                        FLAGS_modular_lossy_palette);
+    }
+    if (modular_palette_colors_set) {
+      JxlEncoderFrameSettingsSetOption(jxl_encoder_frame_settings,
+                                       JXL_ENC_FRAME_SETTING_PALETTE_COLORS,
+                                       FLAGS_modular_palette_colors);
+    }
+    if (modular_channel_colors_global_percent_set) {
+      if (!(-1 <= FLAGS_modular_channel_colors_global_percent &&
+            FLAGS_modular_channel_colors_global_percent <= 100)) {
+        std::cerr << "Invalid --modular_channel_colors_global_percent: "
+                  << FLAGS_modular_channel_colors_global_percent << std::endl;
+        return EXIT_FAILURE;
+      }
+      JxlEncoderFrameSettingsSetOption(
+          jxl_encoder_frame_settings,
+          JXL_ENC_FRAME_SETTING_CHANNEL_COLORS_GLOBAL_PERCENT,
+          FLAGS_modular_channel_colors_global_percent);
+    }
+    if (modular_channel_colors_group_percent_set) {
+      if (!(-1 <= FLAGS_modular_channel_colors_group_percent &&
+            FLAGS_modular_channel_colors_group_percent <= 100)) {
+        std::cerr << "Invalid --modular_channel_colors_group_percent: "
+                  << FLAGS_modular_channel_colors_group_percent << std::endl;
+        return EXIT_FAILURE;
+      }
+      JxlEncoderFrameSettingsSetOption(
+          jxl_encoder_frame_settings,
+          JXL_ENC_FRAME_SETTING_CHANNEL_COLORS_GROUP_PERCENT,
+          FLAGS_modular_channel_colors_group_percent);
     }
   }
   // Color related (not for modular-mode)
@@ -708,7 +791,7 @@ int main(int argc, char** argv) {
       basic_info.animation.tps_numerator = ppf_info.animation.tps_numerator;
       basic_info.animation.tps_denominator = ppf_info.animation.tps_denominator;
       basic_info.animation.num_loops = ppf_info.animation.num_loops;
-      
+
       basic_info.num_extra_channels =
           (ppf.frames[0].color.format.num_channels == 2 ||
            ppf.frames[0].color.format.num_channels == 4)
@@ -736,7 +819,8 @@ int main(int argc, char** argv) {
       }
     }
 
-    for (const jxl::extras::PackedFrame& pframe : ppf.frames) {
+    for (int num_frame = 0; num_frame < ppf.frames.size(); ++num_frame) {
+      const jxl::extras::PackedFrame& pframe = ppf.frames[num_frame];
       const jxl::extras::PackedImage& pimage = pframe.color;
       JxlPixelFormat ppixelformat = pimage.format;
       {
@@ -746,6 +830,11 @@ int main(int argc, char** argv) {
           std::cerr << "JxlEncoderSetFrameHeader() failed.\n";
           return EXIT_FAILURE;
         }
+      }
+      if (num_frame < FLAGS_frame_indexing.size() &&
+          FLAGS_frame_indexing[num_frame] == '1') {
+        JxlEncoderFrameSettingsSetOption(jxl_encoder_frame_settings,
+                                         JXL_ENC_FRAME_INDEX_BOX, 1);
       }
       {
         jxl::Status enc_status =
