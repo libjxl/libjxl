eadb9f70eb83e7f88246431d355b9bc7387fcc6f
Evgenii Kliuchnikov <eustas@google.com>
2022-03-21 14:25:58 +0000

    Fix heap-buffer-overflow

--- lib/jxl/render_pipeline/stage_write.cc
+++ lib/jxl/render_pipeline/stage_write.cc
@@ -289,11 +289,12 @@ class WriteToPixelCallbackStage : public RenderPipelineStage {
     if (ypos >= height_) return;
     const float* line_buffers[4];
     for (size_t c = 0; c < 3; c++) {
-      line_buffers[c] = GetInputRow(input_rows, c, 0);
+      line_buffers[c] = GetInputRow(input_rows, c, 0) - xextra;
     }
     if (has_alpha_) {
-      line_buffers[3] = GetInputRow(input_rows, alpha_c_, 0);
+      line_buffers[3] = GetInputRow(input_rows, alpha_c_, 0) - xextra;
     } else {
+      // No xextra offset; opaque_alpha_ is a way to set all values to 1.0f.
       line_buffers[3] = opaque_alpha_.data();
     }
     // TODO(veluca): SIMD.
@@ -302,14 +303,16 @@ class WriteToPixelCallbackStage : public RenderPipelineStage {
       size_t j = 0;
       size_t ix = 0;
       for (; ix < kMaxPixelsPerCall && ssize_t(ix) + x0 < limit; ix++) {
-        temp[j++] = line_buffers[0][x0 + ix];
-        temp[j++] = line_buffers[1][x0 + ix];
-        temp[j++] = line_buffers[2][x0 + ix];
+        temp[j++] = line_buffers[0][ix];
+        temp[j++] = line_buffers[1][ix];
+        temp[j++] = line_buffers[2][ix];
         if (rgba_) {
-          temp[j++] = line_buffers[3][x0 + ix];
+          temp[j++] = line_buffers[3][ix];
         }
       }
       pixel_callback_(temp, xpos + x0, ypos, ix);
+      for (size_t c = 0; c < 3; c++) line_buffers[c] += kMaxPixelsPerCall;
+      if (has_alpha_) line_buffers[3] += kMaxPixelsPerCall;
     }
   }
 
