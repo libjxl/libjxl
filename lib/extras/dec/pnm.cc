81208752151cb42bfae2256800ad73ed0628df49
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2022-03-25 21:51:27 +0100

    Pfm scale factor (#1278)
    * Reject PFM header where scale factor is 0.0 / -0.0
    
    Upstream pfm specification states:
    
    [...]
    The line consists of a nonzero decimal number, not necessarily an
    integer. If the number is negative, that means the PFM raster is little
    endian. Otherwise, it is big endian.
    [...]
    
    ref:
    * http://netpbm.sourceforge.net/doc/pfm.html
    
    * Add a warning that PFM Scale Factor is not used
    
    Deviate from original PFM specification and simply use Scale Factor to
    check endianness.
    
    [...]
    The scale factor tells the units of the samples in the raster. You use
    somehow it along with some separately understood unit information to
    turn a sample value into something meaningful, such as watts per square
    meter.
    [...]
    
    Ref:
    * http://netpbm.sourceforge.net/doc/pfm.html

--- lib/extras/dec/pnm.cc
+++ lib/extras/dec/pnm.cc
@@ -276,7 +276,12 @@ class Parser {
     // indicate endianness. All software expects nominal range 0..1.
     double scale;
     JXL_RETURN_IF_ERROR(ParseSigned(&scale));
-    header->big_endian = scale >= 0.0;
+    if (scale == 0.0) {
+      return JXL_FAILURE("PFM: bad scale factor value.");
+    } else if (std::abs(scale) != 1.0) {
+      JXL_WARNING("PFM: Discarding non-unit scale factor");
+    }
+    header->big_endian = scale > 0.0;
     header->bits_per_sample = 32;
     header->floating_point = true;
 
