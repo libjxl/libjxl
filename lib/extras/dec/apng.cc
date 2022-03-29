libjxl/libjxl

commit 66836c5313330db6f0c01465d3bb61b6ce86db7e

cjxl_ng: cropped animations (#1223)

lib/extras/dec/apng.cc
@@ -43,6 +43,7 @@
 #include <utility>
 #include <vector>
 
+#include "jxl/codestream_header.h"
 #include "jxl/encode.h"
 #include "lib/jxl/base/compiler_specific.h"
 #include "lib/jxl/base/printf_macros.h"
@@ -670,7 +671,8 @@ Status DecodeImageAPNG(const Span<const uint8_t> bytes,
         has_nontrivial_background && frame.dispose_op != DISPOSE_OP_PREVIOUS;
     size_t x0 = frame.x0;
     size_t y0 = frame.y0;
-
+    size_t xsize = frame.data.xsize;
+    size_t ysize = frame.data.ysize;
     if (previous_frame_should_be_cleared) {
       size_t xs = frame.data.xsize;
       size_t ys = frame.data.ysize;
@@ -710,6 +712,8 @@ Status DecodeImageAPNG(const Span<const uint8_t> bytes,
 
         x0 = px0;
         y0 = py0;
+        xsize = pxs;
+        ysize = pys;
         should_blend = false;
         ppf->frames.emplace_back(std::move(new_data));
       } else {
@@ -718,12 +722,15 @@ Status DecodeImageAPNG(const Span<const uint8_t> bytes,
         memset(blank.pixels(), 0, blank.pixels_size);
         ppf->frames.emplace_back(std::move(blank));
         auto& pframe = ppf->frames.back();
-        pframe.x0 = px0;
-        pframe.y0 = py0;
+        pframe.frame_info.layer_info.crop_x0 = px0;
+        pframe.frame_info.layer_info.crop_y0 = py0;
+        pframe.frame_info.layer_info.xsize = frame.xsize;
+        pframe.frame_info.layer_info.ysize = frame.ysize;
         pframe.frame_info.duration = 0;
-        pframe.blend = false;
-        pframe.use_for_next_frame = true;
-
+        pframe.frame_info.layer_info.have_crop = 0;
+        pframe.frame_info.layer_info.blend_info.blendmode = JXL_BLEND_REPLACE;
+        pframe.frame_info.layer_info.blend_info.source = 0;
+        pframe.frame_info.layer_info.save_as_reference = 1;
         ppf->frames.emplace_back(std::move(frame.data));
       }
     } else {
@@ -731,18 +738,20 @@ Status DecodeImageAPNG(const Span<const uint8_t> bytes,
     }
 
     auto& pframe = ppf->frames.back();
-    pframe.x0 = x0;
-    pframe.y0 = y0;
+    pframe.frame_info.layer_info.crop_x0 = x0;
+    pframe.frame_info.layer_info.crop_y0 = y0;
+    pframe.frame_info.layer_info.xsize = xsize;
+    pframe.frame_info.layer_info.ysize = ysize;
     pframe.frame_info.duration = frame.duration;
-    pframe.blend = should_blend;
-    pframe.use_for_next_frame = use_for_next_frame;
+    pframe.frame_info.layer_info.blend_info.blendmode =
+        should_blend ? JXL_BLEND_BLEND : JXL_BLEND_REPLACE;
+    pframe.frame_info.layer_info.have_crop = 1;
+    pframe.frame_info.layer_info.blend_info.source = should_blend ? 1 : 0;
+    pframe.frame_info.layer_info.blend_info.alpha = 0;
+    pframe.frame_info.layer_info.save_as_reference = use_for_next_frame ? 1 : 0;
 
-    if (has_nontrivial_background &&
-        frame.dispose_op == DISPOSE_OP_BACKGROUND) {
-      previous_frame_should_be_cleared = true;
-    } else {
-      previous_frame_should_be_cleared = false;
-    }
+    previous_frame_should_be_cleared =
+        has_nontrivial_background && frame.dispose_op == DISPOSE_OP_BACKGROUND;
   }
   if (ppf->frames.empty()) return JXL_FAILURE("No frames decoded");
   ppf->frames.back().frame_info.is_last = true;

tools/cjxl_ng_main.cc
@@ -16,6 +16,7 @@
 #include "jxl/encode_cxx.h"
 #include "jxl/thread_parallel_runner.h"
 #include "jxl/thread_parallel_runner_cxx.h"
+#include "jxl/types.h"
 #include "lib/extras/codec.h"
 #include "lib/extras/dec/apng.h"
 #include "lib/extras/dec/color_hints.h"
@@ -24,6 +25,7 @@
 #include "lib/extras/dec/pgx.h"
 #include "lib/extras/dec/pnm.h"
 #include "lib/jxl/base/file_io.h"
+#include "lib/jxl/base/status.h"
 #include "lib/jxl/size_constraints.h"
 
 DECLARE_bool(help);
@@ -842,47 +844,54 @@ int main(int argc, char** argv) {
       }
       jxl::Status enc_status(true);
       {
-        enc_status =
-            JxlEncoderAddImageFrame(jxl_encoder_frame_settings, &ppixelformat,
-                                    pimage.pixels(), pimage.pixels_size);
-        if (JXL_ENC_SUCCESS != enc_status) {
-          // TODO(tfish): Fix such status handling throughout.  We should
-          // have more detail available about what went wrong than what we
-          // currently share with the caller.
-          std::cerr << "JxlEncoderAddImageFrame() failed.\n";
-          return EXIT_FAILURE;
-        }
         if (num_alpha_channels > 0) {
           JxlExtraChannelInfo extra_channel_info;
-          // TODO(tfish): Clarify if it is OK to leave some fields in this
-          // struct un-initialized.
-          extra_channel_info.type = JXL_CHANNEL_ALPHA;
-          extra_channel_info.bits_per_sample = 8;
-          extra_channel_info.exponent_bits_per_sample = 0;
-          extra_channel_info.dim_shift = 0;
-          extra_channel_info.name_length = 0;
-          extra_channel_info.alpha_premultiplied = FLAGS_premultiply;
+          JxlEncoderInitExtraChannelInfo(JXL_CHANNEL_ALPHA,
+                                         &extra_channel_info);
           enc_status = JxlEncoderSetExtraChannelInfo(jxl_encoder, 0,
                                                      &extra_channel_info);
           if (JXL_ENC_SUCCESS != enc_status) {
             std::cerr << "JxlEncoderSetExtraChannelInfo() failed.\n";
             return EXIT_FAILURE;
           }
-          enc_status = JxlEncoderSetExtraChannelBuffer(
-              jxl_encoder_frame_settings, &ppixelformat,
-              pframe.extra_channels[0].pixels(),
-              pframe.extra_channels[0].stride * pframe.extra_channels[0].ysize,
-              0);
+          extra_channel_info.alpha_premultiplied = FLAGS_premultiply;
+
+          // We take the extra channel blend info frame_info, but don't do
+          // clamping.
+          JxlBlendInfo extra_channel_blend_info =
+              pframe.frame_info.layer_info.blend_info;
+          extra_channel_blend_info.clamp = JXL_FALSE;
+          JxlEncoderSetExtraChannelBlendInfo(jxl_encoder_frame_settings, 0,
+                                             &extra_channel_blend_info);
+
+          enc_status =
+              JxlEncoderAddImageFrame(jxl_encoder_frame_settings, &ppixelformat,
+                                      pimage.pixels(), pimage.pixels_size);
           if (JXL_ENC_SUCCESS != enc_status) {
-            std::cerr << "JxlEncoderSetExtraChannelBuffer() failed.\n";
+            // TODO(tfish): Fix such status handling throughout.  We should
+            // have more detail available about what went wrong than what we
+            // currently share with the caller.
+            std::cerr << "JxlEncoderAddImageFrame() failed.\n";
             return EXIT_FAILURE;
+          }
+          // Only set extra channel buffer if is is provided non-interleaved
+          if (!pframe.extra_channels.empty()) {
+            enc_status = JxlEncoderSetExtraChannelBuffer(
+                jxl_encoder_frame_settings, &ppixelformat,
+                pframe.extra_channels[0].pixels(),
+                pframe.extra_channels[0].stride *
+                    pframe.extra_channels[0].ysize,
+                0);
+            if (JXL_ENC_SUCCESS != enc_status) {
+              std::cerr << "JxlEncoderSetExtraChannelBuffer() failed.\n";
+              return EXIT_FAILURE;
+            }
           }
         }
       }
     }
   }
   JxlEncoderCloseInput(jxl_encoder);
-
   // Reading compressed output
   std::vector<uint8_t> compressed;
   compressed.resize(4096);

lib/extras/dec/gif.cc
@@ -12,6 +12,7 @@
 #include <utility>
 #include <vector>
 
+#include "jxl/codestream_header.h"
 #include "lib/jxl/base/compiler_specific.h"
 #include "lib/jxl/sanitizers.h"
 
@@ -257,15 +258,26 @@ Status DecodeImageGIF(Span<const uint8_t> bytes, const ColorHints& color_hints,
     GraphicsControlBlock gcb;
     DGifSavedExtensionToGCB(gif.get(), i, &gcb);
     msan::UnpoisonMemory(&gcb, sizeof(gcb));
-
+    bool is_full_size = total_rect.x0() == 0 && total_rect.y0() == 0 &&
+                        total_rect.xsize() == canvas.color.xsize &&
+                        total_rect.ysize() == canvas.color.ysize;
     if (ppf->info.have_animation) {
       frame->frame_info.duration = gcb.DelayTime;
-      frame->x0 = total_rect.x0();
-      frame->y0 = total_rect.y0();
+      frame->frame_info.layer_info.have_crop = static_cast<int>(!is_full_size);
+      frame->frame_info.layer_info.crop_x0 = total_rect.x0();
+      frame->frame_info.layer_info.crop_y0 = total_rect.y0();
+      frame->frame_info.layer_info.xsize = frame->color.xsize;
+      frame->frame_info.layer_info.ysize = frame->color.ysize;
       if (last_base_was_none) {
         replace = true;
       }
-      frame->blend = !replace;
+      frame->frame_info.layer_info.blend_info.blendmode =
+          replace ? JXL_BLEND_REPLACE : JXL_BLEND_BLEND;
+      // We always only reference at most the last frame
+      frame->frame_info.layer_info.blend_info.source =
+          last_base_was_none ? 0u : 1u;
+      frame->frame_info.layer_info.blend_info.clamp = 1;
+      frame->frame_info.layer_info.blend_info.alpha = 0;
       // TODO(veluca): this could in principle be implemented.
       if (last_base_was_none &&
           (total_rect.x0() != 0 || total_rect.y0() != 0 ||
@@ -278,14 +290,14 @@ Status DecodeImageGIF(Span<const uint8_t> bytes, const ColorHints& color_hints,
       switch (gcb.DisposalMode) {
         case DISPOSE_DO_NOT:
         case DISPOSE_BACKGROUND:
-          frame->use_for_next_frame = true;
+          frame->frame_info.layer_info.save_as_reference = 1u;
           last_base_was_none = false;
           break;
         case DISPOSE_PREVIOUS:
-          frame->use_for_next_frame = false;
+          frame->frame_info.layer_info.save_as_reference = 0u;
           break;
         default:
-          frame->use_for_next_frame = false;
+          frame->frame_info.layer_info.save_as_reference = 0u;
           last_base_was_none = true;
       }
     }

lib/extras/packed_image.h
@@ -116,16 +116,6 @@ class PackedFrame {
   JxlFrameHeader frame_info = {};
   std::string name;
 
-  // Offset of the frame in the image.
-  // TODO(deymo): Add support in the API for this.
-  size_t x0 = 0;
-  size_t y0 = 0;
-
-  // Whether this frame should be blended with the previous one.
-  // TODO(deymo): Maybe add support for this in the API.
-  bool blend = false;
-  bool use_for_next_frame = false;
-
   // The pixel data for the color (or grayscale) channels.
   PackedImage color;
   // Extra channel image data.

lib/extras/packed_image_convert.cc
@@ -117,20 +117,21 @@ Status ConvertPackedPixelFileToCodecInOut(const PackedPixelFile& ppf,
     const Span<const uint8_t> span(
         static_cast<const uint8_t*>(frame.color.pixels()),
         frame.color.pixels_size);
-    Rect frame_rect =
-        Rect(frame.x0, frame.y0, frame.color.xsize, frame.color.ysize);
+    Rect frame_rect = Rect(frame.frame_info.layer_info.crop_x0,
+                           frame.frame_info.layer_info.crop_y0,
+                           frame.frame_info.layer_info.xsize,
+                           frame.frame_info.layer_info.ysize);
     JXL_ASSERT(frame_rect.IsInside(Rect(0, 0, ppf.info.xsize, ppf.info.ysize)));
-
     ImageBundle bundle(&io->metadata.m);
     if (ppf.info.have_animation) {
       bundle.duration = frame.frame_info.duration;
-      bundle.blend = frame.blend;
-      bundle.use_for_next_frame = frame.use_for_next_frame;
+      bundle.blend = frame.frame_info.layer_info.blend_info.blendmode > 0;
+      bundle.use_for_next_frame =
+          frame.frame_info.layer_info.save_as_reference > 0;
+      bundle.origin.x0 = frame.frame_info.layer_info.crop_x0;
+      bundle.origin.y0 = frame.frame_info.layer_info.crop_y0;
     }
     bundle.name = frame.name;  // frame.frame_info.name_length is ignored here.
-    bundle.origin.x0 = frame.x0;
-    bundle.origin.y0 = frame.y0;
-
     JXL_ASSERT(io->metadata.m.color_encoding.IsGray() ==
                (frame.color.format.num_channels <= 2));
 

lib/jxl/encode.cc
@@ -363,9 +363,10 @@ JxlEncoderStatus JxlEncoderStruct::RefillOutputByteQueue() {
         input_frame->option_values.header.layer_info.blend_info.alpha;
     frame_info.extra_channel_blending_info.resize(
         metadata.m.num_extra_channels);
-    // If extra channel blend info has not been set, use the default values.
-    JxlBlendInfo default_blend_info;
-    JxlEncoderInitBlendInfo(&default_blend_info);
+    // If extra channel blend info has not been set, use the blend mode from the
+    // layer_info.
+    JxlBlendInfo default_blend_info =
+        input_frame->option_values.header.layer_info.blend_info;
     for (size_t i = 0; i < metadata.m.num_extra_channels; ++i) {
       auto& to = frame_info.extra_channel_blending_info[i];
       const auto& from =
@@ -375,6 +376,7 @@ JxlEncoderStatus JxlEncoderStruct::RefillOutputByteQueue() {
       to.mode = static_cast<jxl::BlendMode>(from.blendmode);
       to.source = from.source;
       to.alpha_channel = from.alpha;
+      to.clamp = (from.clamp != 0);
     }
 
     if (input_frame->option_values.header.layer_info.have_crop) {
@@ -1247,6 +1249,19 @@ JxlEncoderStatus JxlEncoderAddImageFrame(
       queued_frame->ec_initialized.push_back(0);
     }
   }
+  queued_frame->frame.origin.x0 =
+      frame_settings->values.header.layer_info.crop_x0;
+  queued_frame->frame.origin.y0 =
+      frame_settings->values.header.layer_info.crop_y0;
+  queued_frame->frame.use_for_next_frame =
+      (frame_settings->values.header.layer_info.save_as_reference != 0u);
+  queued_frame->frame.blendmode =
+      frame_settings->values.header.layer_info.blend_info.blendmode ==
+              JXL_BLEND_REPLACE
+          ? jxl::BlendMode::kReplace
+          : jxl::BlendMode::kBlend;
+  queued_frame->frame.blend =
+      frame_settings->values.header.layer_info.blend_info.source > 0;
 
   if (!jxl::BufferToImageBundle(*pixel_format, xsize, ysize, buffer, size,
                                 frame_settings->enc->thread_pool.get(),

commit f7c0a1a837ffa9d92258a7a9ed9b0a2f8390c4d4

cjxl_ng: fixing single-frame pngs
Fixing an issue introduced in #1223. Drive-by: don't always assume
cropping.

lib/extras/dec/apng.cc
@@ -745,7 +745,9 @@ Status DecodeImageAPNG(const Span<const uint8_t> bytes,
     pframe.frame_info.duration = frame.duration;
     pframe.frame_info.layer_info.blend_info.blendmode =
         should_blend ? JXL_BLEND_BLEND : JXL_BLEND_REPLACE;
-    pframe.frame_info.layer_info.have_crop = 1;
+    bool is_full_size = x0 == 0 && y0 == 0 && xsize == ppf->info.xsize &&
+                        ysize == ppf->info.ysize;
+    pframe.frame_info.layer_info.have_crop = is_full_size ? 0 : 1;
     pframe.frame_info.layer_info.blend_info.source = should_blend ? 1 : 0;
     pframe.frame_info.layer_info.blend_info.alpha = 0;
     pframe.frame_info.layer_info.save_as_reference = use_for_next_frame ? 1 : 0;

tools/cjxl_ng_main.cc
@@ -838,7 +838,6 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
       }
     }
-
     for (size_t num_frame = 0; num_frame < ppf.frames.size(); ++num_frame) {
       const jxl::extras::PackedFrame& pframe = ppf.frames[num_frame];
       const jxl::extras::PackedImage& pimage = pframe.color;
@@ -877,17 +876,17 @@ int main(int argc, char** argv) {
           extra_channel_blend_info.clamp = JXL_FALSE;
           JxlEncoderSetExtraChannelBlendInfo(jxl_encoder_frame_settings, 0,
                                              &extra_channel_blend_info);
-
-          enc_status =
-              JxlEncoderAddImageFrame(jxl_encoder_frame_settings, &ppixelformat,
-                                      pimage.pixels(), pimage.pixels_size);
-          if (JXL_ENC_SUCCESS != enc_status) {
-            // TODO(tfish): Fix such status handling throughout.  We should
-            // have more detail available about what went wrong than what we
-            // currently share with the caller.
-            std::cerr << "JxlEncoderAddImageFrame() failed.\n";
-            return EXIT_FAILURE;
-          }
+        }
+        enc_status =
+            JxlEncoderAddImageFrame(jxl_encoder_frame_settings, &ppixelformat,
+                                    pimage.pixels(), pimage.pixels_size);
+        if (JXL_ENC_SUCCESS != enc_status) {
+          // TODO(tfish): Fix such status handling throughout.  We should
+          // have more detail available about what went wrong than what we
+          // currently share with the caller.
+          std::cerr << "JxlEncoderAddImageFrame() failed.\n";
+          return EXIT_FAILURE;
+        }
           // Only set extra channel buffer if is is provided non-interleaved
           if (!pframe.extra_channels.empty()) {
             enc_status = JxlEncoderSetExtraChannelBuffer(
@@ -901,7 +900,6 @@ int main(int argc, char** argv) {
               return EXIT_FAILURE;
             }
           }
-        }
       }
     }
   }