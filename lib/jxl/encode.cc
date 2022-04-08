c46f12986017a83755373727527bb75987b728c7
Evgenii Kliuchnikov <eustas@google.com>
2022-04-08 08:59:30 +0000

    Deal with Schrödinger MSAN report

--- lib/jxl/encode.cc
+++ lib/jxl/encode.cc
@@ -1080,7 +1080,10 @@ int JxlEncoderGetRequiredCodestreamLevel(const JxlEncoder* enc) {
   return VerifyLevelSettings(enc, nullptr);
 }
 
-void JxlEncoderSetCms(JxlEncoder* enc, JxlCmsInterface cms) { enc->cms = cms; }
+void JxlEncoderSetCms(JxlEncoder* enc, JxlCmsInterface cms) {
+  jxl::msan::MemoryIsInitialized(&cms, sizeof(cms));
+  enc->cms = cms;
+}
 
 JxlEncoderStatus JxlEncoderSetParallelRunner(JxlEncoder* enc,
                                              JxlParallelRunner parallel_runner,
