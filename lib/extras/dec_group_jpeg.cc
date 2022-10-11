116143f5054b5f5117a3539ff71da6aa53efd1f9
Zoltan Szabadka <szabadka@google.com>
2022-10-11 14:13:40 +0200

    Remove dependence on libjxl's render pipeline for jpeg decoding.
    We decode the image one MCU-row at a time (8 or 16 rows) instead
    of by 256x256 tiles.
    
    Benchmark before (single-threaded):
    ```
    Encoding                 kPixels    Bytes          BPP  E MP/s  D MP/s     Max norm        pnorm       BPP*pnorm   Bugs
    -----------------------------------------------------------------------------------------------------------------------
    jpeg:q95:djxl8             13270  7112433    4.2876819  55.107  93.795   1.63272071   0.44628983  1.913548813406      0
    jpeg:q95:yuv420:djxl8      13270  5230427    3.1531274  83.334 128.316   6.06236982   0.78961182  2.489746632976      0
    ```
    
    Bnenchmark after:
    ```
    Encoding                 kPixels    Bytes          BPP  E MP/s  D MP/s     Max norm        pnorm       BPP*pnorm   Bugs
    -----------------------------------------------------------------------------------------------------------------------
    jpeg:q95:djxl8             13270  7112433    4.2876819  54.919 104.071   1.63272071   0.44628983  1.913548813406      0
    jpeg:q95:yuv420:djxl8      13270  5230427    3.1531274  82.888 142.408   6.06236982   0.78961033  2.489741937504      0
    ```
    
    Benchmark for libjpeg-turbo:
    ```
    Encoding           kPixels    Bytes          BPP  E MP/s  D MP/s     Max norm        pnorm       BPP*pnorm   Bugs
    -----------------------------------------------------------------------------------------------------------------
    jpeg:q95             13270  7112433    4.2876819  53.544 139.849   1.71719611   0.48347738  2.072997220807      0
    jpeg:q95:yuv420      13270  5230427    3.1531274  81.365 209.858   6.00716972   0.80121163  2.526322308500      0
    ```

--- lib/extras/dec_group_jpeg.cc
+++ lib/extras/dec_group_jpeg.cc
@@ -20,7 +20,9 @@
 #include "lib/jxl/base/status.h"
 #include "lib/jxl/dct_scales.h"
 #include "lib/jxl/dec_transforms-inl.h"
-#include "lib/jxl/render_pipeline/render_pipeline.h"
+#include "lib/jxl/sanitizers.h"
+#include "lib/jxl/simd_util-inl.h"
+
 HWY_BEFORE_NAMESPACE();
 namespace jxl {
 namespace HWY_NAMESPACE {
@@ -29,10 +31,12 @@ namespace HWY_NAMESPACE {
 using hwy::HWY_NAMESPACE::And;
 using hwy::HWY_NAMESPACE::AndNot;
 using hwy::HWY_NAMESPACE::ApproximateReciprocal;
+using hwy::HWY_NAMESPACE::Clamp;
 using hwy::HWY_NAMESPACE::Gt;
 using hwy::HWY_NAMESPACE::IfThenElse;
 using hwy::HWY_NAMESPACE::IfThenElseZero;
 using hwy::HWY_NAMESPACE::Lt;
+using hwy::HWY_NAMESPACE::NearestInt;
 using hwy::HWY_NAMESPACE::Rebind;
 using hwy::HWY_NAMESPACE::Vec;
 using hwy::HWY_NAMESPACE::Xor;
@@ -41,6 +45,13 @@ using D = HWY_FULL(float);
 using DI = HWY_FULL(int32_t);
 constexpr D d;
 constexpr DI di;
+
+static constexpr float kDefaultQuantBias[4] = {
+    1.0f - 0.05465007330715401f,
+    1.0f - 0.07005449891748593f,
+    1.0f - 0.049935103337343655f,
+    0.145f,
+};
 
 template <class DI>
 HWY_INLINE HWY_MAYBE_UNUSED Vec<Rebind<float, DI>> AdjustQuantBias(
@@ -89,47 +100,137 @@ void DequantBlock(const int16_t* JXL_RESTRICT qblock, size_t c,
   }
 }
 
-Status DecodeGroupJpeg(const Image3S& coeffs, size_t group_idx,
-                       const Rect block_rect, const YCbCrChromaSubsampling& cs,
-                       const float* dequant_matrices,
-                       float* JXL_RESTRICT group_dec_cache, size_t thread,
-                       RenderPipelineInput& render_pipeline_input) {
-  HWY_ALIGN float* const block = group_dec_cache;
-  HWY_ALIGN float* const scratch_space = block + kDCTBlockSize;
+void DecodeJpegBlock(const int16_t* qblock, size_t c,
+                     const float* JXL_RESTRICT dequant_matrices,
+                     float* JXL_RESTRICT scratch_space,
+                     float* JXL_RESTRICT output, size_t output_stride) {
+  HWY_ALIGN float* const block = scratch_space + kDCTBlockSize;
+  DequantBlock(qblock, c, dequant_matrices, kDefaultQuantBias, scratch_space);
+  // JPEG XL transposes the DCT, JPEG doesn't.
+  Transpose<8, 8>::Run(DCTFrom(scratch_space, 8), DCTTo(block, 8));
+  TransformToPixels(AcStrategy::DCT, block, output, output_stride,
+                    scratch_space);
+}
 
-  size_t hshift[3] = {cs.HShift(0), cs.HShift(1), cs.HShift(2)};
-  size_t vshift[3] = {cs.VShift(0), cs.VShift(1), cs.VShift(2)};
+void Upsample2Horizontal(float* JXL_RESTRICT row_in,
+                         float* JXL_RESTRICT row_out, size_t len_out) {
+  HWY_FULL(float) df;
+  auto threefour = Set(df, 0.75f);
+  auto onefour = Set(df, 0.25f);
+  const size_t len_in = len_out >> 1;
+  row_in[-1] = row_in[0];
+  row_in[len_in] = row_in[len_in - 1];
+  for (size_t x = 0; x < len_in; x += Lanes(df)) {
+    auto current = Mul(Load(df, row_in + x), threefour);
+    auto prev = LoadU(df, row_in + x - 1);
+    auto next = LoadU(df, row_in + x + 1);
+    auto left = MulAdd(onefour, prev, current);
+    auto right = MulAdd(onefour, next, current);
+    StoreInterleaved(df, left, right, row_out + x * 2);
+  }
+}
 
-  static constexpr float kDefaultQuantBias[4] = {
-      1.0f - 0.05465007330715401f,
-      1.0f - 0.07005449891748593f,
-      1.0f - 0.049935103337343655f,
-      0.145f,
-  };
+void Upsample2Vertical(const float* JXL_RESTRICT row_top,
+                       const float* JXL_RESTRICT row_mid,
+                       const float* JXL_RESTRICT row_bot,
+                       float* JXL_RESTRICT row_out0,
+                       float* JXL_RESTRICT row_out1, size_t len) {
+  HWY_FULL(float) df;
+  auto threefour = Set(df, 0.75f);
+  auto onefour = Set(df, 0.25f);
+  for (size_t x = 0; x < len; x += Lanes(df)) {
+    auto it = Load(df, row_top + x);
+    auto im = Load(df, row_mid + x);
+    auto ib = Load(df, row_bot + x);
+    auto im_scaled = Mul(im, threefour);
+    Store(MulAdd(it, onefour, im_scaled), df, row_out0 + x);
+    Store(MulAdd(ib, onefour, im_scaled), df, row_out1 + x);
+  }
+}
 
-  for (size_t c = 0; c < 3; ++c) {
-    ImageF* rpbuffer = render_pipeline_input.GetBuffer(c).first;
-    Rect rect = render_pipeline_input.GetBuffer(c).second;
-    size_t xsize_blocks = DivCeil(block_rect.xsize(), 1 << hshift[c]);
-    size_t ysize_blocks = DivCeil(block_rect.ysize(), 1 << vshift[c]);
-    size_t offset = 0;
-    for (size_t by = 0; by < ysize_blocks; ++by) {
-      float* JXL_RESTRICT idct_row = rect.Row(rpbuffer, by * kBlockDim);
-      size_t idct_stride = rpbuffer->PixelsPerRow();
-      for (size_t bx = 0; bx < xsize_blocks; ++bx) {
-        const int16_t* qblock = &coeffs.PlaneRow(c, group_idx)[offset];
-        offset += kDCTBlockSize;
-        DequantBlock(qblock, c, dequant_matrices, kDefaultQuantBias, block);
-        // IDCT
-        float* JXL_RESTRICT idct_pos = idct_row + bx * kBlockDim;
-        // JPEG XL transposes the DCT, JPEG doesn't.
-        Transpose<8, 8>::Run(DCTFrom(block, 8), DCTTo(scratch_space, 8));
-        TransformToPixels(AcStrategy::DCT, scratch_space, idct_pos, idct_stride,
-                          block);
-      }
+void YCbCrToRGB(float* JXL_RESTRICT row0, float* JXL_RESTRICT row1,
+                float* JXL_RESTRICT row2, size_t xsize) {
+  const HWY_FULL(float) df;
+
+  // Full-range BT.601 as defined by JFIF Clause 7:
+  // https://www.itu.int/rec/T-REC-T.871-201105-I/en
+  const auto c128 = Set(df, 128.0f / 255);
+  const auto crcr = Set(df, 1.402f);
+  const auto cgcb = Set(df, -0.114f * 1.772f / 0.587f);
+  const auto cgcr = Set(df, -0.299f * 1.402f / 0.587f);
+  const auto cbcb = Set(df, 1.772f);
+
+  for (size_t x = 0; x < xsize; x += Lanes(df)) {
+    const auto y_vec = Add(Load(df, row1 + x), c128);
+    const auto cb_vec = Load(df, row0 + x);
+    const auto cr_vec = Load(df, row2 + x);
+    const auto r_vec = MulAdd(crcr, cr_vec, y_vec);
+    const auto g_vec = MulAdd(cgcr, cr_vec, MulAdd(cgcb, cb_vec, y_vec));
+    const auto b_vec = MulAdd(cbcb, cb_vec, y_vec);
+    Store(r_vec, df, row0 + x);
+    Store(g_vec, df, row1 + x);
+    Store(b_vec, df, row2 + x);
+  }
+}
+
+void DecenterRow(float* row, size_t xsize) {
+  const HWY_FULL(float) df;
+  const auto c128 = Set(df, 128.0f / 255);
+  for (size_t x = 0; x < xsize; x += Lanes(df)) {
+    Store(Add(Load(df, row + x), c128), df, row + x);
+  }
+}
+
+template <typename T>
+void StoreUnsignedRow(float* JXL_RESTRICT input[3], size_t x0, size_t len,
+                      size_t num_channels, T* output) {
+  const HWY_FULL(float) d;
+  auto zero = Zero(d);
+  auto one = Set(d, 1.0f);
+  auto mul = Set(d, (1u << (sizeof(T) * 8)) - 1);
+  const Rebind<T, decltype(d)> du;
+  const size_t padding = RoundUpTo(len, Lanes(d)) - len;
+  for (size_t c = 0; c < num_channels; ++c) {
+    msan::UnpoisonMemory(input[c] + x0 + len, sizeof(input[c][0]) * padding);
+  }
+  if (num_channels == 1) {
+    for (size_t i = 0; i < len; i += Lanes(d)) {
+      auto v0 = Mul(Clamp(zero, Load(d, &input[0][x0 + i]), one), mul);
+      Store(DemoteTo(du, NearestInt(v0)), du, &output[i]);
+    }
+  } else if (num_channels == 3) {
+    for (size_t i = 0; i < len; i += Lanes(d)) {
+      auto v0 = Mul(Clamp(zero, Load(d, &input[0][x0 + i]), one), mul);
+      auto v1 = Mul(Clamp(zero, Load(d, &input[1][x0 + i]), one), mul);
+      auto v2 = Mul(Clamp(zero, Load(d, &input[2][x0 + i]), one), mul);
+      StoreInterleaved3(DemoteTo(du, NearestInt(v0)),
+                        DemoteTo(du, NearestInt(v1)),
+                        DemoteTo(du, NearestInt(v2)), du, &output[3 * i]);
     }
   }
-  return true;
+  msan::PoisonMemory(output + num_channels * len,
+                     sizeof(output[0]) * num_channels * padding);
+}
+
+void WriteToPackedImage(float* JXL_RESTRICT rows[3], size_t x0, size_t y0,
+                        size_t len, uint8_t* JXL_RESTRICT scratch_space,
+                        extras::PackedImage* image) {
+  if (y0 >= image->ysize) return;
+  JxlPixelFormat format = image->format;
+  uint8_t* pixels = reinterpret_cast<uint8_t*>(image->pixels());
+  if (format.data_type == JXL_TYPE_UINT8) {
+    size_t offset = y0 * image->stride + x0 * format.num_channels;
+    JXL_CHECK(offset + len * format.num_channels <= image->pixels_size);
+    StoreUnsignedRow(rows, x0, len, format.num_channels, scratch_space);
+    memcpy(pixels + offset, scratch_space, len * format.num_channels);
+  } else if (format.data_type == JXL_TYPE_UINT16) {
+    size_t offset = y0 * image->stride + x0 * format.num_channels * 2;
+    JXL_CHECK(offset + len * format.num_channels * 2 <= image->pixels_size);
+    uint16_t* tmp = reinterpret_cast<uint16_t*>(scratch_space);
+    StoreUnsignedRow(rows, x0, len, format.num_channels, tmp);
+    // TODO(szabadka) Handle endianness.
+    memcpy(pixels + offset, tmp, len * format.num_channels * 2);
+  }
 }
 
 // NOLINTNEXTLINE(google-readability-namespace-comments)
@@ -140,18 +241,51 @@ HWY_AFTER_NAMESPACE();
 #if HWY_ONCE
 namespace jxl {
 namespace {
-HWY_EXPORT(DecodeGroupJpeg);
+HWY_EXPORT(DecodeJpegBlock);
+HWY_EXPORT(Upsample2Horizontal);
+HWY_EXPORT(Upsample2Vertical);
+HWY_EXPORT(YCbCrToRGB);
+HWY_EXPORT(DecenterRow);
+HWY_EXPORT(WriteToPackedImage);
 }  // namespace
 
 namespace extras {
-Status DecodeGroupJpeg(const Image3S& coeffs, size_t group_idx,
-                       const Rect block_rect, const YCbCrChromaSubsampling& cs,
-                       const float* dequant_matrices,
-                       float* JXL_RESTRICT group_dec_cache, size_t thread,
-                       RenderPipelineInput& render_pipeline_input) {
-  return HWY_DYNAMIC_DISPATCH(DecodeGroupJpeg)(
-      coeffs, group_idx, block_rect, cs, dequant_matrices, group_dec_cache,
-      thread, render_pipeline_input);
+void DecodeJpegBlock(const int16_t* qblock, size_t c,
+                     const float* JXL_RESTRICT dequant_matrices,
+                     float* JXL_RESTRICT scratch_space,
+                     float* JXL_RESTRICT output, size_t output_stride) {
+  return HWY_DYNAMIC_DISPATCH(DecodeJpegBlock)(
+      qblock, c, dequant_matrices, scratch_space, output, output_stride);
+}
+
+void Upsample2Horizontal(float* JXL_RESTRICT row_in,
+                         float* JXL_RESTRICT row_out, size_t len_out) {
+  return HWY_DYNAMIC_DISPATCH(Upsample2Horizontal)(row_in, row_out, len_out);
+}
+
+void Upsample2Vertical(const float* JXL_RESTRICT row_top,
+                       const float* JXL_RESTRICT row_mid,
+                       const float* JXL_RESTRICT row_bot,
+                       float* JXL_RESTRICT row_out0,
+                       float* JXL_RESTRICT row_out1, size_t len) {
+  return HWY_DYNAMIC_DISPATCH(Upsample2Vertical)(row_top, row_mid, row_bot,
+                                                 row_out0, row_out1, len);
+}
+
+void YCbCrToRGB(float* JXL_RESTRICT row0, float* JXL_RESTRICT row1,
+                float* JXL_RESTRICT row2, size_t xsize) {
+  return HWY_DYNAMIC_DISPATCH(YCbCrToRGB)(row0, row1, row2, xsize);
+}
+
+void DecenterRow(float* row, size_t xsize) {
+  return HWY_DYNAMIC_DISPATCH(DecenterRow)(row, xsize);
+}
+
+void WriteToPackedImage(float* JXL_RESTRICT rows[3], size_t x0, size_t y0,
+                        size_t len, uint8_t* JXL_RESTRICT scratch_space,
+                        extras::PackedImage* image) {
+  return HWY_DYNAMIC_DISPATCH(WriteToPackedImage)(rows, x0, y0, len,
+                                                  scratch_space, image);
 }
 
 }  // namespace extras
