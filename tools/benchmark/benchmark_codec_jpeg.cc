416705666b78c8a63241f9a2020ce176f6ddf5e0
szabadka <9074039+szabadka@users.noreply.github.com>
2022-11-20 15:19:39 +0100

    Add option to run custom cjpeg command in jpeg benchmark. (#1905)
    With this addition, one can benchmark against a decoder-only libpeg
    implementation by setting a custom LD_LIBRARY_PATH and using a
    statically built cjpeg in the benchmark.

--- tools/benchmark/benchmark_codec_jpeg.cc
+++ tools/benchmark/benchmark_codec_jpeg.cc
@@ -22,10 +22,12 @@
 #include "lib/extras/packed_image.h"
 #include "lib/extras/packed_image_convert.h"
 #include "lib/extras/time.h"
+#include "lib/jxl/base/file_io.h"
 #include "lib/jxl/base/padded_bytes.h"
 #include "lib/jxl/base/span.h"
 #include "lib/jxl/base/thread_pool_internal.h"
 #include "lib/jxl/codec_in_out.h"
+#include "tools/benchmark/benchmark_utils.h"
 #include "tools/cmdline.h"
 
 namespace jxl {
@@ -60,7 +62,8 @@ class JPEGCodec : public ImageCodec {
     if (ImageCodec::ParseParam(param)) {
       return true;
     }
-    if (param == "sjpeg" || param == "libjxl") {
+    if (param == "sjpeg" || param == "libjxl" ||
+        param.find("cjpeg") != std::string::npos) {
       jpeg_encoder_ = param;
       return true;
     }
@@ -93,6 +96,45 @@ class JPEGCodec : public ImageCodec {
   Status Compress(const std::string& filename, const CodecInOut* io,
                   ThreadPoolInternal* pool, std::vector<uint8_t>* compressed,
                   jpegxl::tools::SpeedStats* speed_stats) override {
+    if (jpeg_encoder_.find("cjpeg") != std::string::npos) {
+// Not supported on Windows due to Linux-specific functions.
+// Not supported in Android NDK before API 28.
+#if !defined(_WIN32) && !defined(__EMSCRIPTEN__) && \
+    (!defined(__ANDROID_API__) || __ANDROID_API__ >= 28)
+      const std::string basename = GetBaseName(filename);
+      TemporaryFile in_file(basename, "pnm");
+      TemporaryFile encoded_file(basename, "jpg");
+      std::string in_filename, encoded_filename;
+      JXL_RETURN_IF_ERROR(in_file.GetFileName(&in_filename));
+      JXL_RETURN_IF_ERROR(encoded_file.GetFileName(&encoded_filename));
+      const size_t bits = io->metadata.m.bit_depth.bits_per_sample;
+      ColorEncoding c_enc = io->Main().c_current();
+      JXL_RETURN_IF_ERROR(EncodeToFile(*io, c_enc, bits, in_filename, pool));
+      std::string compress_command = jpeg_encoder_;
+      std::vector<std::string> arguments;
+      arguments.push_back("-outfile");
+      arguments.push_back(encoded_filename);
+      arguments.push_back("-quality");
+      arguments.push_back(std::to_string(static_cast<int>(q_target_)));
+      arguments.push_back("-sample");
+      if (chroma_subsampling_ == "444") {
+        arguments.push_back("1x1");
+      } else if (chroma_subsampling_ == "420") {
+        arguments.push_back("2x2");
+      } else {
+        return JXL_FAILURE("Unsupported chroma subsampling");
+      }
+      arguments.push_back("-optimize");
+      arguments.push_back(in_filename);
+      const double start = Now();
+      JXL_RETURN_IF_ERROR(RunCommand(compress_command, arguments, false));
+      const double end = Now();
+      speed_stats->NotifyElapsed(end - start);
+      return ReadFile(encoded_filename, compressed);
+#else
+      return JXL_FAILURE("Not supported on this build");
+#endif
+    }
     double elapsed = 0.0;
     if (jpeg_encoder_ != "libjxl" || normalize_bitrate_) {
       extras::PackedPixelFile ppf;
