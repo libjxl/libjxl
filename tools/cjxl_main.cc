2a95fe4d03080ffd82b654d0745f7d9f4e2cda94
Moritz Firsching <firsching@google.com>
2022-12-21 08:47:44 +0100

    disallow nonsensible lossless_jpeg settings (#1991)
    This fixes #1968. We disallow the combination `--lossless_jpeg=1` with
    `-d` for non-zero distances or `-q` for qualities not equal to 100.

--- tools/cjxl_main.cc
+++ tools/cjxl_main.cc
@@ -632,6 +632,14 @@ void SetDistanceFromFlags(CommandLineParser* cmdline, CompressArgs* args,
                           const jxl::extras::Codec& codec) {
   bool distance_set = cmdline->GetOption(args->opt_distance_id)->matched();
   bool quality_set = cmdline->GetOption(args->opt_quality_id)->matched();
+  if (((distance_set && (args->distance != 0.0)) ||
+       (quality_set && (args->quality != 100))) &&
+      args->lossless_jpeg && args->lossless_jpeg) {
+    std::cerr << "Must not set quality below 100 nor non-zero distance in "
+                 "combination with --lossless_jpeg=1."
+              << std::endl;
+    exit(EXIT_FAILURE);
+  }
   if (quality_set) {
     if (distance_set) {
       std::cerr << "Must not set both --distance and --quality." << std::endl;
