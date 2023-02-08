2aa4945306ce54fff1f7648da5a2ae562ea3d0dd
Zoltan Szabadka <szabadka@google.com>
2023-02-08 10:51:38 +0100

    Fix msan failure.

--- lib/jpegli/adaptive_quantization.cc
+++ lib/jpegli/adaptive_quantization.cc
@@ -648,12 +648,14 @@ jxl::ImageF InitialQuantField(const float butteraugli_target,
 
 void ComputeAdaptiveQuantField(j_compress_ptr cinfo) {
   jpeg_comp_master* m = cinfo->master;
-  m->quant_field.Allocate(m->ysize_blocks, m->xsize_blocks);
   int y_channel = cinfo->jpeg_color_space == JCS_RGB ? 1 : 0;
+  jpeg_component_info* y_comp = &cinfo->comp_info[y_channel];
+  m->quant_field.Allocate(y_comp->height_in_blocks, y_comp->width_in_blocks);
   if (m->use_adaptive_quantization &&
-      cinfo->comp_info[y_channel].h_samp_factor == cinfo->max_h_samp_factor &&
-      cinfo->comp_info[y_channel].v_samp_factor == cinfo->max_v_samp_factor) {
-    jxl::ImageF input(m->xsize_blocks * DCTSIZE, m->ysize_blocks * DCTSIZE);
+      y_comp->h_samp_factor == cinfo->max_h_samp_factor &&
+      y_comp->v_samp_factor == cinfo->max_v_samp_factor) {
+    jxl::ImageF input(y_comp->width_in_blocks * DCTSIZE,
+                      y_comp->height_in_blocks * DCTSIZE);
     for (size_t y = 0; y < input.ysize(); ++y) {
       memcpy(input.Row(y), m->input_buffer[y_channel].Row(y),
              input.xsize() * sizeof(float));
@@ -662,14 +664,14 @@ void ComputeAdaptiveQuantField(j_compress_ptr cinfo) {
         jpegli::InitialQuantField(m->distance, input, nullptr, m->distance);
     float qfmin;
     ImageMinMax(qf, &qfmin, &m->quant_field_max);
-    for (size_t y = 0; y < m->ysize_blocks; ++y) {
-      m->quant_field.CopyRow(y, qf.Row(y), m->xsize_blocks);
+    for (size_t y = 0; y < y_comp->height_in_blocks; ++y) {
+      m->quant_field.CopyRow(y, qf.Row(y), y_comp->width_in_blocks);
     }
   } else {
     constexpr float kDefaultQuantFieldMax = 0.575f;
     m->quant_field_max = kDefaultQuantFieldMax;
-    for (size_t y = 0; y < m->ysize_blocks; ++y) {
-      m->quant_field.FillRow(y, m->quant_field_max, m->xsize_blocks);
+    for (size_t y = 0; y < y_comp->height_in_blocks; ++y) {
+      m->quant_field.FillRow(y, m->quant_field_max, y_comp->width_in_blocks);
     }
   }
 }
