dffe90de5a851a575965d3540717f944fa9c9a72
Moritz Firsching <firsching@google.com>
2023-05-26 11:14:01 +0200

    wip

--- lib/jxl/jxl_cms.h
+++ lib/jxl/jxl_cms.h
@@ -4,6 +4,9 @@
 // license that can be found in the LICENSE file.
 #ifndef LIB_JXL_JXL_CMS_H_
 #define LIB_JXL_JXL_CMS_H_
+
 #include "lib/jxl/color_management.h"
+
+const JxlCmsInterface& GetJxlCms();
 
 #endif
