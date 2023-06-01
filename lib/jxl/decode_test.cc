bd796144c70b0e7c32fb36752b8c47cef35fe782
Moritz Firsching <firsching@google.com>
2023-06-01 11:32:14 +0200

    fix PixelTestWithICCProfileLossy
    caused by #2503, this fixes a problem when cross-compiling

--- lib/jxl/decode_test.cc
+++ lib/jxl/decode_test.cc
@@ -1677,7 +1677,7 @@ TEST(DecodeTest, PixelTestWithICCProfileLossy) {
   jxl::ButteraugliParams ba;
   EXPECT_THAT(ButteraugliDistance(io0.frames, io1.frames, ba, jxl::GetJxlCms(),
                                   /*distmap=*/nullptr, nullptr),
-              IsSlightlyBelow(0.89898f));
+              IsSlightlyBelow(0.9273f));
 
   JxlDecoderDestroy(dec);
 }
