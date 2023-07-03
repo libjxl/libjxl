9e1399dde09cd908d1716f96e42633832b477087
Jon Sneyers <jon@cloudinary.com>
2023-07-03 16:12:21 +0200

    further tweaks

--- lib/jxl/enc_adaptive_quantization.cc
+++ lib/jxl/enc_adaptive_quantization.cc
@@ -630,38 +630,43 @@ HWY_EXPORT(AdaptiveQuantizationMap);
 
 namespace {
 
-#if JXL_DEBUG_ADAPTIVE_QUANTIZATION
+// If true, prints the quantization maps at each iteration.
+constexpr bool FLAGS_dump_quant_state = false;
+
 void DumpHeatmap(const AuxOut* aux_out, const std::string& label,
                  const ImageF& image, float good_threshold,
                  float bad_threshold) {
-  Image3F heatmap = CreateHeatMapImage(image, good_threshold, bad_threshold);
-  char filename[200];
-  snprintf(filename, sizeof(filename), "%s%05d", label.c_str(),
-           aux_out->num_butteraugli_iters);
-  aux_out->DumpImage(filename, heatmap);
+  if (JXL_DEBUG_ADAPTIVE_QUANTIZATION) {
+    Image3F heatmap = CreateHeatMapImage(image, good_threshold, bad_threshold);
+    char filename[200];
+    snprintf(filename, sizeof(filename), "%s%05d", label.c_str(),
+             aux_out->num_butteraugli_iters);
+    aux_out->DumpImage(filename, heatmap);
+  }
 }
 
 void DumpHeatmaps(const AuxOut* aux_out, float ba_target,
                   const ImageF& quant_field, const ImageF& tile_heatmap,
                   const ImageF& bt_diffmap) {
-  if (!WantDebugOutput(aux_out)) return;
-  ImageF inv_qmap(quant_field.xsize(), quant_field.ysize());
-  for (size_t y = 0; y < quant_field.ysize(); ++y) {
-    const float* JXL_RESTRICT row_q = quant_field.ConstRow(y);
-    float* JXL_RESTRICT row_inv_q = inv_qmap.Row(y);
-    for (size_t x = 0; x < quant_field.xsize(); ++x) {
-      row_inv_q[x] = 1.0f / row_q[x];  // never zero
+  if (JXL_DEBUG_ADAPTIVE_QUANTIZATION) {
+    if (!WantDebugOutput(aux_out)) return;
+    ImageF inv_qmap(quant_field.xsize(), quant_field.ysize());
+    for (size_t y = 0; y < quant_field.ysize(); ++y) {
+      const float* JXL_RESTRICT row_q = quant_field.ConstRow(y);
+      float* JXL_RESTRICT row_inv_q = inv_qmap.Row(y);
+      for (size_t x = 0; x < quant_field.xsize(); ++x) {
+        row_inv_q[x] = 1.0f / row_q[x];  // never zero
+      }
     }
+    DumpHeatmap(aux_out, "quant_heatmap", inv_qmap, 4.0f * ba_target,
+                6.0f * ba_target);
+    DumpHeatmap(aux_out, "tile_heatmap", tile_heatmap, ba_target,
+                1.5f * ba_target);
+    // matches heat maps produced by the command line tool.
+    DumpHeatmap(aux_out, "bt_diffmap", bt_diffmap, ButteraugliFuzzyInverse(1.5),
+                ButteraugliFuzzyInverse(0.5));
   }
-  DumpHeatmap(aux_out, "quant_heatmap", inv_qmap, 4.0f * ba_target,
-              6.0f * ba_target);
-  DumpHeatmap(aux_out, "tile_heatmap", tile_heatmap, ba_target,
-              1.5f * ba_target);
-  // matches heat maps produced by the command line tool.
-  DumpHeatmap(aux_out, "bt_diffmap", bt_diffmap, ButteraugliFuzzyInverse(1.5),
-              ButteraugliFuzzyInverse(0.5));
 }
-#endif
 
 ImageF TileDistMap(const ImageF& distmap, int tile_size, int margin,
                    const AcStrategyImage& ac_strategy) {
@@ -883,15 +888,15 @@ void FindBestQuantization(const ImageBundle& linear, const Image3F& opsin,
     iters = 2;
   }
   for (int i = 0; i < iters + 1; ++i) {
-#if JXL_DEBUG_ADAPTIVE_QUANTIZATION
-    printf("\nQuantization field:\n");
-    for (size_t y = 0; y < quant_field.ysize(); ++y) {
-      for (size_t x = 0; x < quant_field.xsize(); ++x) {
-        printf(" %.5f", quant_field.Row(y)[x]);
+    if (JXL_DEBUG_ADAPTIVE_QUANTIZATION) {
+      printf("\nQuantization field:\n");
+      for (size_t y = 0; y < quant_field.ysize(); ++y) {
+        for (size_t x = 0; x < quant_field.xsize(); ++x) {
+          printf(" %.5f", quant_field.Row(y)[x]);
+        }
+        printf("\n");
       }
-      printf("\n");
     }
-#endif
     quantizer.SetQuantField(initial_quant_dc, quant_field, &raw_quant_field);
     ImageBundle dec_linear = RoundtripImage(opsin, enc_state, cms, pool);
     float score;
@@ -903,26 +908,24 @@ void FindBestQuantization(const ImageBundle& linear, const Image3F& opsin,
     }
     tile_distmap = TileDistMap(diffmap, 8 * cparams.resampling, 0,
                                enc_state->shared.ac_strategy);
-#if JXL_DEBUG_ADAPTIVE_QUANTIZATION
-    if (WantDebugOutput(aux_out)) {
+    if (JXL_DEBUG_ADAPTIVE_QUANTIZATION && WantDebugOutput(aux_out)) {
       aux_out->DumpImage(("dec" + ToString(i)).c_str(), *dec_linear.color());
       DumpHeatmaps(aux_out, butteraugli_target, quant_field, tile_distmap,
                    diffmap);
     }
-#endif
     if (aux_out != nullptr) ++aux_out->num_butteraugli_iters;
-#if JXL_DEBUG_ADAPTIVE_QUANTIZATION
-    float minval, maxval;
-    ImageMinMax(quant_field, &minval, &maxval);
-    printf("\nButteraugli iter: %d/%d\n", i, cparams.max_butteraugli_iters);
-    printf("Butteraugli distance: %f  (target = %f)\n", score,
-           original_butteraugli);
-    printf("quant range: %f ... %f  DC quant: %f\n", minval, maxval,
-           initial_quant_dc);
-    if (FLAGS_dump_quant_state) {
-      quantizer.DumpQuantizationMap(raw_quant_field);
+    if (JXL_DEBUG_ADAPTIVE_QUANTIZATION) {
+      float minval, maxval;
+      ImageMinMax(quant_field, &minval, &maxval);
+      printf("\nButteraugli iter: %d/%d\n", i, cparams.max_butteraugli_iters);
+      printf("Butteraugli distance: %f  (target = %f)\n", score,
+             original_butteraugli);
+      printf("quant range: %f ... %f  DC quant: %f\n", minval, maxval,
+             initial_quant_dc);
+      if (FLAGS_dump_quant_state) {
+        quantizer.DumpQuantizationMap(raw_quant_field);
+      }
     }
-#endif
 
     if (i == iters) break;
 
@@ -1026,17 +1029,13 @@ void FindBestQuantizationMaxError(const Image3F& opsin,
 
   for (int i = 0; i < cparams.max_butteraugli_iters + 1; ++i) {
     quantizer.SetQuantField(initial_quant_dc, quant_field, &raw_quant_field);
-#if JXL_DEBUG_ADAPTIVE_QUANTIZATION
-    if (aux_out) {
+    if (JXL_DEBUG_ADAPTIVE_QUANTIZATION && aux_out) {
       aux_out->DumpXybImage(("ops" + ToString(i)).c_str(), opsin);
     }
-#endif
     ImageBundle decoded = RoundtripImage(opsin, enc_state, cms, pool);
-#if JXL_DEBUG_ADAPTIVE_QUANTIZATION
-    if (aux_out) {
+    if (JXL_DEBUG_ADAPTIVE_QUANTIZATION && aux_out) {
       aux_out->DumpXybImage(("dec" + ToString(i)).c_str(), *decoded.color());
     }
-#endif
     for (size_t by = 0; by < enc_state->shared.frame_dim.ysize_blocks; by++) {
       AcStrategyRow ac_strategy_row =
           enc_state->shared.ac_strategy.ConstRow(by);
--- lib/jxl/enc_aux_out.cc
+++ lib/jxl/enc_aux_out.cc
@@ -60,18 +60,19 @@ const char* LayerName(size_t layer) {
 }
 
 void AuxOut::LayerTotals::Print(size_t num_inputs) const {
-#if JXL_DEBUG_V_LEVEL > 0
-  printf("%10" PRId64, static_cast<int64_t>(total_bits));
-  if (histogram_bits != 0) {
-    printf("   [c/i:%6.2f | hst:%8" PRId64 " | ex:%8" PRId64 " | h+c+e:%12.3f",
-           num_clustered_histograms * 1.0 / num_inputs,
-           static_cast<int64_t>(histogram_bits >> 3),
-           static_cast<int64_t>(extra_bits >> 3),
-           (histogram_bits + clustered_entropy + extra_bits) / 8.0);
-    printf("]");
+  if (JXL_DEBUG_V_LEVEL > 0) {
+    printf("%10" PRId64, static_cast<int64_t>(total_bits));
+    if (histogram_bits != 0) {
+      printf("   [c/i:%6.2f | hst:%8" PRId64 " | ex:%8" PRId64
+             " | h+c+e:%12.3f",
+             num_clustered_histograms * 1.0 / num_inputs,
+             static_cast<int64_t>(histogram_bits >> 3),
+             static_cast<int64_t>(extra_bits >> 3),
+             (histogram_bits + clustered_entropy + extra_bits) / 8.0);
+      printf("]");
+    }
+    printf("\n");
   }
-  printf("\n");
-#endif
 }
 
 void AuxOut::Assimilate(const AuxOut& victim) {
@@ -102,55 +103,55 @@ void AuxOut::Assimilate(const AuxOut& victim) {
 }
 
 void AuxOut::Print(size_t num_inputs) const {
-#if JXL_DEBUG_V_LEVEL > 0
-  if (num_inputs == 0) return;
+  if (JXL_DEBUG_V_LEVEL > 0) {
+    if (num_inputs == 0) return;
 
-  LayerTotals all_layers;
-  for (size_t i = 0; i < layers.size(); ++i) {
-    all_layers.Assimilate(layers[i]);
-  }
+    LayerTotals all_layers;
+    for (size_t i = 0; i < layers.size(); ++i) {
+      all_layers.Assimilate(layers[i]);
+    }
 
-  printf("Average butteraugli iters: %10.2f\n",
-         num_butteraugli_iters * 1.0 / num_inputs);
-  if (min_quant_rescale != 1.0 || max_quant_rescale != 1.0) {
-    printf("quant rescale range: %f .. %f\n", min_quant_rescale,
-           max_quant_rescale);
-    printf("bitrate error range: %.3f%% .. %.3f%%\n",
-           100.0f * min_bitrate_error, 100.0f * max_bitrate_error);
-  }
+    printf("Average butteraugli iters: %10.2f\n",
+           num_butteraugli_iters * 1.0 / num_inputs);
+    if (min_quant_rescale != 1.0 || max_quant_rescale != 1.0) {
+      printf("quant rescale range: %f .. %f\n", min_quant_rescale,
+             max_quant_rescale);
+      printf("bitrate error range: %.3f%% .. %.3f%%\n",
+             100.0f * min_bitrate_error, 100.0f * max_bitrate_error);
+    }
 
-  for (size_t i = 0; i < layers.size(); ++i) {
-    if (layers[i].total_bits != 0) {
-      printf("Total layer bits %-10s\t", LayerName(i));
-      printf("%10f%%", 100.0 * layers[i].total_bits / all_layers.total_bits);
-      layers[i].Print(num_inputs);
+    for (size_t i = 0; i < layers.size(); ++i) {
+      if (layers[i].total_bits != 0) {
+        printf("Total layer bits %-10s\t", LayerName(i));
+        printf("%10f%%", 100.0 * layers[i].total_bits / all_layers.total_bits);
+        layers[i].Print(num_inputs);
+      }
+    }
+    printf("Total image size           ");
+    all_layers.Print(num_inputs);
+
+    const uint32_t dc_pred_total =
+        std::accumulate(dc_pred_usage.begin(), dc_pred_usage.end(), 0u);
+    const uint32_t dc_pred_total_xb =
+        std::accumulate(dc_pred_usage_xb.begin(), dc_pred_usage_xb.end(), 0u);
+    if (dc_pred_total + dc_pred_total_xb != 0) {
+      printf("\nDC pred     Y                XB:\n");
+      for (size_t i = 0; i < dc_pred_usage.size(); ++i) {
+        printf("  %6u (%5.2f%%)    %6u (%5.2f%%)\n", dc_pred_usage[i],
+               100.0 * dc_pred_usage[i] / dc_pred_total, dc_pred_usage_xb[i],
+               100.0 * dc_pred_usage_xb[i] / dc_pred_total_xb);
+      }
+    }
+
+    size_t total_blocks = 0;
+    size_t total_positions = 0;
+    if (total_blocks != 0 && total_positions != 0) {
+      printf("\n\t\t  Blocks\t\tPositions\t\t\tBlocks/Position\n");
+      printf(" Total:\t\t    %7" PRIuS "\t\t     %7" PRIuS " \t\t\t%10f%%\n\n",
+             total_blocks, total_positions,
+             100.0 * total_blocks / total_positions);
     }
   }
-  printf("Total image size           ");
-  all_layers.Print(num_inputs);
-
-  const uint32_t dc_pred_total =
-      std::accumulate(dc_pred_usage.begin(), dc_pred_usage.end(), 0u);
-  const uint32_t dc_pred_total_xb =
-      std::accumulate(dc_pred_usage_xb.begin(), dc_pred_usage_xb.end(), 0u);
-  if (dc_pred_total + dc_pred_total_xb != 0) {
-    printf("\nDC pred     Y                XB:\n");
-    for (size_t i = 0; i < dc_pred_usage.size(); ++i) {
-      printf("  %6u (%5.2f%%)    %6u (%5.2f%%)\n", dc_pred_usage[i],
-             100.0 * dc_pred_usage[i] / dc_pred_total, dc_pred_usage_xb[i],
-             100.0 * dc_pred_usage_xb[i] / dc_pred_total_xb);
-    }
-  }
-
-  size_t total_blocks = 0;
-  size_t total_positions = 0;
-  if (total_blocks != 0 && total_positions != 0) {
-    printf("\n\t\t  Blocks\t\tPositions\t\t\tBlocks/Position\n");
-    printf(" Total:\t\t    %7" PRIuS "\t\t     %7" PRIuS " \t\t\t%10f%%\n\n",
-           total_blocks, total_positions,
-           100.0 * total_blocks / total_positions);
-  }
-#endif
 }
 
 template <typename T>
