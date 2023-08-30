6c2944eb7ad702066ff45afa5cbcacc546acc579
Moritz Firsching <firsching@google.com>
2023-08-30 21:20:13 +0200

    try initialization

--- lib/jxl/transfer_functions-inl.h
+++ lib/jxl/transfer_functions-inl.h
@@ -165,7 +165,7 @@ class TF_709 {
 // Perceptual Quantization
 class TF_PQ {
  public:
-  explicit TF_PQ(float display_intensity_target)
+  explicit TF_PQ(float display_intensity_target = 255.0)
       : display_scaling_factor_to_10000_nits_(display_intensity_target *
                                               (1.0f / 10000.0f)),
         display_scaling_factor_from_10000_nits_(10000.0f /
