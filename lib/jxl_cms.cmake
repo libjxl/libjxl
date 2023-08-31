f8a3c6b69cb70c46c0a2a04d14c2ba333fb1dd7a
Moritz Firsching <firsching@google.com>
2023-08-31 14:18:26 +0200

    target_link_libraries

--- lib/jxl_cms.cmake
+++ lib/jxl_cms.cmake
@@ -30,7 +30,7 @@ add_library(jxl_cms OBJECT
 
 target_compile_options(jxl_cms PRIVATE "${JPEGXL_INTERNAL_FLAGS}")
 set_target_properties(jxl_cms PROPERTIES POSITION_INDEPENDENT_CODE ON)
-target_include_directories(jxl_cms PRIVATE $<TARGET_PROPERTY:jxl_includes,INCLUDE_DIRECTORIES>)
+target_link_directories(jxl_cms PRIVATE jxl_includes)
 add_dependencies(jxl_cms jxl_cms_export)
 
 target_include_directories(jxl_cms PUBLIC
