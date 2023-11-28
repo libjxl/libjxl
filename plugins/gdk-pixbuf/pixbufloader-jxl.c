a67f0b7fe3a8ea79a6cc1cbbf32da597391e4672
Moritz Firsching <firsching@google.com>
2023-11-28 17:03:10 +0100

    use JxlEncoderDistanceFromQuality in pixbufloader

--- plugins/gdk-pixbuf/pixbufloader-jxl.c
+++ plugins/gdk-pixbuf/pixbufloader-jxl.c
@@ -684,12 +684,7 @@ static gboolean jxl_image_saver(FILE *f, GdkPixbuf *pixbuf, gchar **keys,
     distance = 0;
   } else {
     output_info.uses_original_profile = JXL_FALSE;
-    if (quality >= 30) {
-      distance = 0.1 + (100 - quality) * 0.09;
-    } else {
-      distance =
-          53.0 / 3000.0 * quality * quality - 23.0 / 20.0 * quality + 25.0;
-    }
+    distance = JxlEncoderDistanceFromQuality((float)quality);
   }
 
   status = JxlEncoderSetBasicInfo(encoder, &output_info);
