cbd44009f664536f6040512f0a2c75b2112a32c2
Luca Versari <veluca93@gmail.com>
2023-12-13 20:56:19 +0100

    Add a basic flake.nix file for development on NixOS. (#3019)
    Run `nix develop` to get a dev environment.
    
    (cherry picked from commit 59d9573dd3d5e8520f2464911883bf0b58a28e02)

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [
+              clang
+              cmake
+              pkg-config
+              gtest
+              gmock
+              doxygen
+              graphviz
+            ];
+            shellHook = ''
+              export CC=clang
+              export CXX=clang++
+            '';
+          };
+        }
+      );
+}
