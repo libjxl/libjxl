693cf3a498a4c3bd63f64aced125c27e8c2c2de0
BtbN <btbn@btbn.de>
2023-12-27 16:45:13 +0100

    Fix link (#3045)
    * Fix linking order of jxl_cms dependencies
    
    * Use pkg-config dependency for external lcms2
    
    * Add myself to AUTHORS
    
    * Update lib/jxl_cms.cmake
    
    * Update libjxl_cms.pc.in
    
    ---------
    
    Co-authored-by: Eugene Kliuchnikov <eustas@google.com>
    Co-authored-by: Moritz Firsching <firsching@google.com>
    Co-authored-by: Luca Versari <veluca@google.com>

--- lib/jxl_cms.cmake
+++ lib/jxl_cms.cmake
@@ -23,14 +23,14 @@ generate_export_header(jxl_cms
 target_include_directories(jxl_cms PUBLIC
   "$<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}>")
 
-set(JXL_CMS_PK_LIBS "")
+set(JPEGXL_CMS_LIBRARY_REQUIRES "")
 
 if (JPEGXL_ENABLE_SKCMS)
   target_link_libraries(jxl_cms PRIVATE skcms)
 else()
   target_link_libraries(jxl_cms PRIVATE lcms2)
   if (JPEGXL_FORCE_SYSTEM_LCMS2)
-    set(JXL_CMS_PK_LIBS "-llcms2")
+    set(JPEGXL_CMS_LIBRARY_REQUIRES "lcms2")
   endif()
 endif()
 
@@ -63,7 +63,6 @@ else()
   set(JPEGXL_REQUIRES_TYPE "Requires")
 endif()
 
-set(JPEGXL_CMS_LIBRARY_REQUIRES "")
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/jxl/libjxl_cms.pc.in"
                "libjxl_cms.pc" @ONLY)
 install(FILES "${CMAKE_CURRENT_BINARY_DIR}/libjxl_cms.pc"
