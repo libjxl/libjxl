libjxl/libjxl

commit 946c4112659ef7b4692966f428065e122c708e06

Remove some more uses of CodecInOut (#3180)

tools/decode_and_encode.cc
@@ -11,7 +11,6 @@
 #include "lib/jxl/base/data_parallel.h"
 #include "lib/jxl/base/span.h"
 #include "lib/jxl/base/status.h"
-#include "lib/jxl/codec_in_out.h"
 #include "tools/file_io.h"
 #include "tools/thread_pool_internal.h"
 
@@ -20,8 +19,8 @@ namespace {
 // Reads an input file (typically PNM) with color_space hint and writes to an
 // output file (typically PNG) which supports all required metadata.
 int Convert(int argc, char** argv) {
-  if (argc != 4 && argc != 5) {
-    fprintf(stderr, "Args: in colorspace_description out [bits]\n");
+  if (argc != 4) {
+    fprintf(stderr, "Args: in colorspace_description out\n");
     return 1;
   }
   const std::string& pathname_in = argv[1];
@@ -33,17 +32,17 @@ int Convert(int argc, char** argv) {
     fprintf(stderr, "Failed to read image from %s\n", pathname_in.c_str());
     return 1;
   }
-  jxl::CodecInOut io;
+  jxl::extras::PackedPixelFile ppf;
   jxl::extras::ColorHints color_hints;
   jpegxl::tools::ThreadPoolInternal pool(4);
   color_hints.Add("color_space", desc);
-  if (!jxl::SetFromBytes(jxl::Bytes(encoded_in), color_hints, &io, &pool)) {
+  if (!jxl::extras::DecodeBytes(jxl::Bytes(encoded_in), color_hints, &ppf)) {
     fprintf(stderr, "Failed to decode %s\n", pathname_in.c_str());
     return 1;
   }
 
   std::vector<uint8_t> encoded_out;
-  if (!jxl::Encode(io, pathname_out, &encoded_out, &pool)) {
+  if (!jxl::Encode(ppf, pathname_out, &encoded_out, &pool)) {
     fprintf(stderr, "Failed to encode %s\n", pathname_out.c_str());
     return 1;
   }

lib/jxl/decode_test.cc
@@ -33,6 +33,7 @@
 
 #include "lib/extras/codec.h"
 #include "lib/extras/dec/color_description.h"
+#include "lib/extras/enc/jpg.h"
 #include "lib/jxl/base/byte_order.h"
 #include "lib/jxl/base/common.h"
 #include "lib/jxl/base/compiler_specific.h"
@@ -293,9 +294,20 @@ std::vector<uint8_t> CreateTestJXLCodestream(
   if (params.jpeg_codestream != nullptr) {
     if (jxl::extras::CanDecode(jxl::extras::Codec::kJPG)) {
       std::vector<uint8_t> jpeg_bytes;
-      io.jpeg_quality = 70;
-      EXPECT_TRUE(Encode(io, extras::Codec::kJPG, io.metadata.m.color_encoding,
-                         /*bits_per_sample=*/8, &jpeg_bytes));
+      extras::PackedPixelFile ppf;
+      extras::PackedFrame frame(xsize, ysize, format);
+      JXL_ASSERT(frame.color.pixels_size == pixels.size());
+      memcpy(frame.color.pixels(0, 0, 0), pixels.data(), pixels.size());
+      ppf.frames.emplace_back(std::move(frame));
+      ppf.info.xsize = xsize;
+      ppf.info.ysize = ysize;
+      ppf.info.num_color_channels = grayscale ? 1 : 3;
+      ppf.info.bits_per_sample = 16;
+      auto encoder = extras::GetJPEGEncoder();
+      encoder->SetOption("quality", "70");
+      extras::EncodedImage encoded;
+      EXPECT_TRUE(encoder->Encode(ppf, &encoded));
+      jpeg_bytes = encoded.bitstreams[0];
       Bytes(jpeg_bytes).AppendTo(params.jpeg_codestream);
       EXPECT_TRUE(jxl::jpeg::DecodeImageJPG(
           jxl::Bytes(jpeg_bytes.data(), jpeg_bytes.size()), &io));

tools/benchmark/benchmark_codec_custom.cc
@@ -119,15 +119,6 @@ class CustomCodec : public ImageCodec {
   Status Compress(const std::string& filename, const PackedPixelFile& ppf,
                   ThreadPool* pool, std::vector<uint8_t>* compressed,
                   jpegxl::tools::SpeedStats* speed_stats) override {
-    CodecInOut io;
-    JXL_RETURN_IF_ERROR(
-        jxl::extras::ConvertPackedPixelFileToCodecInOut(ppf, pool, &io));
-    return Compress(filename, &io, pool, compressed, speed_stats);
-  }
-
-  Status Compress(const std::string& filename, const CodecInOut* io,
-                  ThreadPool* pool, std::vector<uint8_t>* compressed,
-                  jpegxl::tools::SpeedStats* speed_stats) {
     JXL_RETURN_IF_ERROR(param_index_ > 2);
 
     const std::string basename = GetBaseName(filename);
@@ -136,18 +127,9 @@ class CustomCodec : public ImageCodec {
     std::string in_filename, encoded_filename;
     JXL_RETURN_IF_ERROR(in_file.GetFileName(&in_filename));
     JXL_RETURN_IF_ERROR(encoded_file.GetFileName(&encoded_filename));
-    saved_intensity_target_ = io->metadata.m.IntensityTarget();
-
-    const size_t bits = io->metadata.m.bit_depth.bits_per_sample;
-    ColorEncoding c_enc = io->Main().c_current();
-    if (!custom_args->colorspace.empty()) {
-      JxlColorEncoding colorspace;
-      JXL_RETURN_IF_ERROR(
-          jxl::ParseDescription(custom_args->colorspace, &colorspace));
-      JXL_RETURN_IF_ERROR(c_enc.FromExternal(colorspace));
-    }
+    // TODO(szabadka) Support custom_args->colorspace again.
     std::vector<uint8_t> encoded;
-    JXL_RETURN_IF_ERROR(Encode(*io, c_enc, bits, in_filename, &encoded, pool));
+    JXL_RETURN_IF_ERROR(Encode(ppf, in_filename, &encoded, pool));
     JXL_RETURN_IF_ERROR(WriteFile(in_filename, encoded));
     std::vector<std::string> arguments = compress_args_;
     arguments.push_back(in_filename);

tools/butteraugli_main.cc
@@ -15,6 +15,7 @@
 #include "lib/extras/codec.h"
 #include "lib/extras/dec/color_hints.h"
 #include "lib/extras/metrics.h"
+#include "lib/extras/packed_image_convert.h"
 #include "lib/jxl/base/printf_macros.h"
 #include "lib/jxl/base/span.h"
 #include "lib/jxl/base/status.h"
@@ -36,15 +37,14 @@ using jxl::Image3F;
 using jxl::ImageF;
 using jxl::Status;
 
-Status WriteImage(Image3F&& image, const std::string& filename) {
+Status WriteImage(const Image3F& image, const std::string& filename) {
   ThreadPoolInternal pool(4);
-  CodecInOut io;
-  io.metadata.m.SetUintSamples(8);
-  io.metadata.m.color_encoding = ColorEncoding::SRGB();
-  io.SetFromImage(std::move(image), io.metadata.m.color_encoding);
-
+  JxlPixelFormat format = {3, JXL_TYPE_UINT8, JXL_LITTLE_ENDIAN, 0};
+  jxl::extras::PackedPixelFile ppf =
+      jxl::extras::ConvertImage3FToPackedPixelFile(
+          image, jxl::ColorEncoding::SRGB(), format, &pool);
   std::vector<uint8_t> encoded;
-  return jxl::Encode(io, filename, &encoded, &pool) &&
+  return jxl::Encode(ppf, filename, &encoded, &pool) &&
          jpegxl::tools::WriteFile(filename, encoded);
 }
 

tools/djxl_fuzzer_corpus.cc
@@ -33,6 +33,8 @@
 #include <vector>
 
 #include "lib/extras/codec.h"
+#include "lib/extras/enc/encode.h"
+#include "lib/extras/enc/jpg.h"
 #include "lib/jxl/base/data_parallel.h"
 #include "lib/jxl/base/override.h"
 #include "lib/jxl/base/span.h"
@@ -199,6 +201,11 @@ bool GenerateFile(const char* output_dir, const ImageSpec& spec,
   std::uniform_int_distribution<> dis(1, 6);
   PixelGenerator gen = [&]() -> uint8_t { return dis(mt); };
 
+  jxl::extras::PackedPixelFile ppf;
+  ppf.info.xsize = spec.width;
+  ppf.info.ysize = spec.height;
+  ppf.info.num_color_channels = spec.num_channels ? 1 : 3;
+  ppf.info.bits_per_sample = spec.bit_depth;
   for (uint32_t frame = 0; frame < spec.num_frames; frame++) {
     jxl::ImageBundle ib(&io.metadata.m);
     const bool has_alpha = spec.alpha_bit_depth != 0;
@@ -226,6 +233,11 @@ bool GenerateFile(const char* output_dir, const ImageSpec& spec,
         span, spec.width, spec.height, io.metadata.m.color_encoding,
         io.metadata.m.bit_depth.bits_per_sample, format, nullptr, &ib));
     io.frames.push_back(std::move(ib));
+    jxl::extras::PackedFrame packed_frame(spec.width, spec.height, format);
+    JXL_ASSERT(packed_frame.color.pixels_size == img_data.size());
+    memcpy(packed_frame.color.pixels(0, 0, 0), img_data.data(),
+           img_data.size());
+    ppf.frames.emplace_back(std::move(packed_frame));
   }
 
   jxl::CompressParams params;
@@ -236,10 +248,11 @@ bool GenerateFile(const char* output_dir, const ImageSpec& spec,
     // metadata and encode it in the beginning of the compressed bytes.
     std::vector<uint8_t> jpeg_bytes;
     io.jpeg_quality = 70;
-    JXL_QUIET_RETURN_IF_ERROR(jxl::Encode(io, jxl::extras::Codec::kJPG,
-                                          io.metadata.m.color_encoding,
-                                          /*bits_per_sample=*/8, &jpeg_bytes,
-                                          /*pool=*/nullptr));
+    auto encoder = jxl::extras::GetJPEGEncoder();
+    encoder->SetOption("quality", "70");
+    jxl::extras::EncodedImage encoded;
+    JXL_RETURN_IF_ERROR(encoder->Encode(ppf, &encoded));
+    jpeg_bytes = encoded.bitstreams[0];
     JXL_RETURN_IF_ERROR(jxl::jpeg::DecodeImageJPG(
         jxl::Bytes(jpeg_bytes.data(), jpeg_bytes.size()), &io));
     std::vector<uint8_t> jpeg_data;

tools/hdr/generate_lut_template.cc
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 
 #include "lib/extras/codec.h"
+#include "lib/extras/packed_image_convert.h"
 #include "lib/jxl/image_metadata.h"
 #include "tools/args.h"
 #include "tools/cmdline.h"
@@ -53,11 +54,11 @@ int main(int argc, const char** argv) {
       },
       "GenerateTemplate"));
 
-  jxl::CodecInOut output;
-  output.metadata.m.bit_depth.bits_per_sample = 16;
-  output.SetFromImage(std::move(image), jxl::ColorEncoding::SRGB());
+  JxlPixelFormat format = {3, JXL_TYPE_UINT16, JXL_BIG_ENDIAN, 0};
+  jxl::extras::PackedPixelFile ppf =
+      jxl::extras::ConvertImage3FToPackedPixelFile(
+          image, jxl::ColorEncoding::SRGB(), format, &pool);
   std::vector<uint8_t> encoded;
-  JXL_CHECK(jxl::Encode(output, jxl::ColorEncoding::SRGB(), 16, output_filename,
-                        &encoded, &pool));
+  JXL_CHECK(jxl::Encode(ppf, output_filename, &encoded, &pool));
   JXL_CHECK(jpegxl::tools::WriteFile(output_filename, encoded));
 }

tools/hdr/local_tone_map.cc
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 
 #include "lib/extras/codec.h"
+#include "lib/extras/packed_image_convert.h"
 #include "lib/extras/tone_mapping.h"
 #include "lib/jxl/convolve.h"
 #include "lib/jxl/enc_gamma_correct.h"
@@ -533,9 +534,10 @@ int main(int argc, const char** argv) {
       std::move(input_images), num_levels, contrast_weight, saturation_weight,
       midtoneness_weight, midtoneness_sigma, &pool);
 
-  jxl::CodecInOut output;
-  output.SetFromImage(std::move(fused), jxl::ColorEncoding::SRGB());
-
-  JXL_CHECK(jxl::Encode(output, output_filename, &encoded, &pool));
+  JxlPixelFormat format = {3, JXL_TYPE_UINT8, JXL_LITTLE_ENDIAN, 0};
+  jxl::extras::PackedPixelFile ppf =
+      jxl::extras::ConvertImage3FToPackedPixelFile(
+          fused, jxl::ColorEncoding::SRGB(), format, &pool);
+  JXL_CHECK(jxl::Encode(ppf, output_filename, &encoded, &pool));
   JXL_CHECK(jpegxl::tools::WriteFile(output_filename, encoded));
 }

commit c6cc69709d1c8444179fe8c2495e3bf1e725b48f

Fix decode_and_encode
It was broken by #3180

tools/decode_and_encode.cc
@@ -41,6 +41,16 @@ int Convert(int argc, char** argv) {
     return 1;
   }
 
+  jxl::ColorEncoding internal;
+  if (!internal.FromExternal(ppf.color_encoding) || internal.ICC().empty()) {
+    fprintf(stderr,
+            "Failed to generate ICC profile from colorspace description\n");
+    return 1;
+  }
+  // Roundtrip so that the chromaticities are populated even for enum values.
+  ppf.color_encoding = internal.ToExternal();
+  ppf.icc = internal.ICC();
+
   std::vector<uint8_t> encoded_out;
   if (!jxl::Encode(ppf, pathname_out, &encoded_out, &pool)) {
     fprintf(stderr, "Failed to encode %s\n", pathname_out.c_str());