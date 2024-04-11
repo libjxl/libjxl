e14fddb86a6c1b75bd8fd4c1e37f9dc5d6cb0117
Moritz Firsching <firsching@google.com>
2024-04-11 15:59:52 +0200

    fix tsan error in ModularFrameEncoder

--- lib/jxl/enc_modular.cc
+++ lib/jxl/enc_modular.cc
@@ -994,11 +994,14 @@ Status ModularFrameEncoder::ComputeEncodingData(
   }
   gi_channel_.resize(stream_images_.size());
 
+  for (size_t i = 1; i < stream_params_.size(); ++i) {
+    size_t stream = stream_params_[i].id.ID(frame_dim_);
+    stream_options_[stream] = stream_options_[0];
+  }
+
   JXL_RETURN_IF_ERROR(RunOnPool(
       pool, 0, stream_params_.size(), ThreadPool::NoInit,
       [&](const uint32_t i, size_t /* thread */) {
-        size_t stream = stream_params_[i].id.ID(frame_dim_);
-        stream_options_[stream] = stream_options_[0];
         JXL_CHECK(PrepareStreamParams(
             stream_params_[i].rect, cparams_, stream_params_[i].minShift,
             stream_params_[i].maxShift, stream_params_[i].id, do_color,
