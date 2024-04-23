9844261261be638b31e9fdb3e6e60bbadf0b08b1
Sergey Fedorov <vital.had@gmail.com>
2024-04-23 20:24:07 +0800

    benchmark_utils: use correct environ on macOS

--- tools/benchmark/benchmark_utils.cc
+++ tools/benchmark/benchmark_utils.cc
@@ -24,7 +24,12 @@
 #include "lib/jxl/image_bundle.h"
 #include "tools/file_io.h"
 
-extern char** environ;  // NOLINT
+#ifdef __APPLE__
+  #include <crt_externs.h>
+  #define environ (*_NSGetEnviron())
+#else
+  extern char **environ;  // NOLINT
+#endif
 
 namespace jpegxl {
 namespace tools {
