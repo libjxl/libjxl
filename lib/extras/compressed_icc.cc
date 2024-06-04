d24d751a146e96a78811b6befe32a5352fd9b96d
Sami Boukortt <sboukortt@google.com>
2024-06-04 16:06:14 +0200

    Implement reading

--- lib/extras/compressed_icc.cc
+++ lib/extras/compressed_icc.cc
@@ -6,6 +6,7 @@
 #include <jxl/compressed_icc.h>
 
 #include "lib/jxl/enc_icc_codec.h"
+#include "lib/jxl/icc_codec.h"
 
 JXL_BOOL JxlIccProfileEncode(JxlMemoryManager* memory_manager,
                              const uint8_t* icc, size_t icc_size,
@@ -26,5 +27,15 @@ JXL_BOOL JxlIccProfileDecode(JxlMemoryManager* memory_manager,
                              const uint8_t* compressed_icc,
                              size_t compressed_icc_size, uint8_t** icc,
                              size_t* icc_size) {
+  jxl::ICCReader icc_reader(memory_manager);
+  jxl::PaddedBytes decompressed(memory_manager);
+  jxl::BitReader bit_reader(
+      jxl::Span<const uint8_t>(compressed_icc, compressed_icc_size));
+  JXL_RETURN_IF_ERROR(icc_reader.Init(&bit_reader, /*output_limit=*/0));
+  JXL_RETURN_IF_ERROR(icc_reader.Process(&bit_reader, &decompressed));
+  *icc_size = decompressed.size();
+  *icc = static_cast<uint8_t*>(
+      memory_manager->alloc(memory_manager->opaque, *icc_size));
+  memcpy(*icc, decompressed.data(), *icc_size);
   return JXL_TRUE;
 }
