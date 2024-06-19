17d1e97d23d6f173d929f6a30547fd8e73784325
Sami Boukortt <sboukortt@google.com>
2024-06-19 15:28:28 +0200

    Fix compressed_icc with memory manager with null fields

--- lib/extras/compressed_icc.cc
+++ lib/extras/compressed_icc.cc
@@ -9,27 +9,35 @@
 #include "lib/jxl/enc_icc_codec.h"
 #include "lib/jxl/icc_codec.h"
 
-JXL_BOOL JxlICCProfileEncode(JxlMemoryManager* memory_manager,
+JXL_BOOL JxlICCProfileEncode(const JxlMemoryManager* memory_manager,
                              const uint8_t* icc, size_t icc_size,
                              uint8_t** compressed_icc,
                              size_t* compressed_icc_size) {
-  jxl::BitWriter writer(memory_manager);
+  JxlMemoryManager local_memory_manager;
+  if (!jxl::MemoryManagerInit(&local_memory_manager, memory_manager)) {
+    return JXL_FALSE;
+  }
+  jxl::BitWriter writer(&local_memory_manager);
   JXL_RETURN_IF_ERROR(jxl::WriteICC(jxl::Span<const uint8_t>(icc, icc_size),
                                     &writer, jxl::LayerType::Header, nullptr));
   writer.ZeroPadToByte();
   *compressed_icc_size = writer.GetSpan().size();
   *compressed_icc = static_cast<uint8_t*>(
-      memory_manager->alloc(memory_manager->opaque, *compressed_icc_size));
+      jxl::MemoryManagerAlloc(&local_memory_manager, *compressed_icc_size));
   memcpy(*compressed_icc, writer.GetSpan().data(), *compressed_icc_size);
   return JXL_TRUE;
 }
 
-JXL_BOOL JxlICCProfileDecode(JxlMemoryManager* memory_manager,
+JXL_BOOL JxlICCProfileDecode(const JxlMemoryManager* memory_manager,
                              const uint8_t* compressed_icc,
                              size_t compressed_icc_size, uint8_t** icc,
                              size_t* icc_size) {
-  jxl::ICCReader icc_reader(memory_manager);
-  jxl::PaddedBytes decompressed(memory_manager);
+  JxlMemoryManager local_memory_manager;
+  if (!jxl::MemoryManagerInit(&local_memory_manager, memory_manager)) {
+    return JXL_FALSE;
+  }
+  jxl::ICCReader icc_reader(&local_memory_manager);
+  jxl::PaddedBytes decompressed(&local_memory_manager);
   jxl::BitReader bit_reader(
       jxl::Span<const uint8_t>(compressed_icc, compressed_icc_size));
   JXL_RETURN_IF_ERROR(icc_reader.Init(&bit_reader, /*output_limit=*/0));
@@ -37,7 +45,7 @@ JXL_BOOL JxlICCProfileDecode(JxlMemoryManager* memory_manager,
   JXL_RETURN_IF_ERROR(bit_reader.Close());
   *icc_size = decompressed.size();
   *icc = static_cast<uint8_t*>(
-      memory_manager->alloc(memory_manager->opaque, *icc_size));
+      jxl::MemoryManagerAlloc(&local_memory_manager, *icc_size));
   memcpy(*icc, decompressed.data(), *icc_size);
   return JXL_TRUE;
 }
