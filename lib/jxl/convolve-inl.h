d23d936acba45307ea771d6c19d318d24cdb57f3
Sami Boukortt <sboukortt@google.com>
2024-09-10 15:03:14 +0200

    Add a couple of missing HWY_ATTR (#3810)
    This might address #3807.
    
    (cherry picked from commit 95123121c1e3a0f3a8961d153480aae9864cbf99)

--- lib/jxl/convolve-inl.h
+++ lib/jxl/convolve-inl.h
@@ -242,8 +242,8 @@ class ConvolveT {
                                          const Weights& weights,
                                          ThreadPool* pool, ImageF* out) {
     const int64_t stride = in.PixelsPerRow();
-    const auto process_row = [&](const uint32_t y,
-                                 size_t /*thread*/) -> Status {
+    const auto process_row = [&](const uint32_t y, size_t /*thread*/)
+                                 HWY_ATTR -> Status {
       RunRow<kSizeModN>(rect.ConstRow(in, y), rect.xsize(), stride,
                         WrapRowUnchanged(), weights, out->Row(y));
       return true;
@@ -262,8 +262,8 @@ class ConvolveT {
                                          const Weights& weights,
                                          ThreadPool* pool, Image3F* out) {
     const int64_t stride = in.PixelsPerRow();
-    const auto process_row = [&](const uint32_t y,
-                                 size_t /*thread*/) -> Status {
+    const auto process_row = [&](const uint32_t y, size_t /*thread*/)
+                                 HWY_ATTR -> Status {
       for (size_t c = 0; c < 3; ++c) {
         RunRow<kSizeModN>(rect.ConstPlaneRow(in, c, y), rect.xsize(), stride,
                           WrapRowUnchanged(), weights, out->PlaneRow(c, y));
