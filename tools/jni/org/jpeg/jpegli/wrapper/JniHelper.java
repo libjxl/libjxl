538c140df1e5390ce51b90a81058c53ca5af5aad
Eugene Kliuchnikov <eustas@google.com>
2024-11-16 11:40:26 +0100

    Fix linter warnings in JniHelpers (#3933)

--- tools/jni/org/jpeg/jpegli/wrapper/JniHelper.java
+++ tools/jni/org/jpeg/jpegli/wrapper/JniHelper.java
@@ -5,6 +5,8 @@
 
 package org.jpeg.jpegli.wrapper;
 
+import java.io.File;
+
 /**
  * Helper for native library loading.
  *
@@ -12,17 +14,20 @@ package org.jpeg.jpegli.wrapper;
  * sometimes necessary to make the moment of loading more deterministic.
  */
 public class JniHelper {
+  /** Utility library, disable object construction. */
+  private JniHelper() {}
+
   static {
     String jniLibrary = System.getProperty("org.jpeg.jpegli.wrapper.lib");
     if (jniLibrary != null) {
       try {
-        System.load(new java.io.File(jniLibrary).getAbsolutePath());
+        System.load(new File(jniLibrary).getAbsolutePath());
       } catch (UnsatisfiedLinkError ex) {
         String message = "If the nested exception message says that some standard library (stdc++, "
             + "tcmalloc, etc.) was not found, it is likely that JDK discovered by the "
             + "build system overrides library search path. Try specifying a different "
             + "JDK via JAVA_HOME environment variable and doing a clean build.";
-        throw new RuntimeException(message, ex);
+        throw new IllegalStateException(message, ex);
       }
     }
   }
