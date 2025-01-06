abe99f409a7a72d82b6c2230f43fb3064d10961f
Evgenii Kliuchnikov <eustas@google.com>
2025-01-06 10:14:40 +0000

    Add DCT/IDCT gbench

--- lib/gbench_main.cc
+++ lib/gbench_main.cc
@@ -5,4 +5,19 @@
 
 #include "benchmark/benchmark.h"
 
-BENCHMARK_MAIN();
+void RegisterDctBenchmarks();
+
+int main(int argc, char** argv) {
+  char arg0_default[] = "benchmark";
+  char* args_default = arg0_default;
+  if (!argv) {
+    argc = 1;
+    argv = &args_default;
+  }
+  RegisterDctBenchmarks();
+  ::benchmark ::Initialize(&argc, argv);
+  if (::benchmark ::ReportUnrecognizedArguments(argc, argv)) return 1;
+  ::benchmark ::RunSpecifiedBenchmarks();
+  ::benchmark ::Shutdown();
+  return 0;
+}
