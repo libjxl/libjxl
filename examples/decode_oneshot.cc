6d848a4ab86cf569d97528ea71e9e526fe09b45d
Evgenii Kliuchnikov <eustas@google.com>
2025-01-09 16:12:40 +0000

    Run release build on 3 clang and 3 gcc versions

--- examples/decode_oneshot.cc
+++ examples/decode_oneshot.cc
@@ -30,9 +30,10 @@ bool DecodeJpegXlOneShot(const uint8_t* jxl, size_t size,
                          std::vector<float>* pixels, size_t* xsize,
                          size_t* ysize, std::vector<uint8_t>* icc_profile) {
   // Multi-threaded parallel runner.
-  auto runner = JxlResizableParallelRunnerMake(nullptr);
+  JxlResizableParallelRunnerPtr runner =
+      JxlResizableParallelRunnerMake(nullptr);
 
-  auto dec = JxlDecoderMake(nullptr);
+  JxlDecoderPtr dec = JxlDecoderMake(nullptr);
   if (JXL_DEC_SUCCESS !=
       JxlDecoderSubscribeEvents(dec.get(), JXL_DEC_BASIC_INFO |
                                                JXL_DEC_COLOR_ENCODING |
