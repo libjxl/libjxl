libjxl/libjxl

commit 02185e1590d809272143a7eb8d19803955ea1669

More precise histogram (#4132)
* More precise histogram

* Review suggestion by @jonsneyers

lib/jxl/enc_ans.cc
@@ -115,16 +115,18 @@ const uint8_t kLogCountSymbols[ANS_LOG_TAB_SIZE + 2] = {
 
 // Returns the difference between largest count that can be represented and is
 // smaller than "count" and smallest representable count larger than "count".
-int SmallestIncrement(uint32_t count, uint32_t shift) {
-  int bits = count == 0 ? -1 : FloorLog2Nonzero(count);
-  int drop_bits = bits - GetPopulationCountPrecision(bits, shift);
-  return drop_bits < 0 ? 1 : (1 << drop_bits);
+uint32_t SmallestIncrementNonzero(uint32_t count, uint32_t shift) {
+  JXL_DASSERT(count > 0);
+  uint32_t bits = FloorLog2Nonzero(count);
+  uint32_t drop_bits = bits - GetPopulationCountPrecision(bits, shift);
+  return 1u << drop_bits;
 }
 
 template <bool minimize_error_of_sum>
-bool RebalanceHistogram(const float* targets, int max_symbol, int table_size,
-                        uint32_t shift, int* omit_pos, ANSHistBin* counts) {
-  int sum = 0;
+bool RebalanceHistogram(const float* targets, int max_symbol,
+                        uint32_t table_size, uint32_t shift, int* omit_pos,
+                        ANSHistBin* counts) {
+  uint32_t sum = 0;
   float sum_nonrounded = 0.0;
   int remainder_pos = 0;  // if all of them are handled in first loop
   int remainder_log = -1;
@@ -132,7 +134,7 @@ bool RebalanceHistogram(const float* targets, int max_symbol, int table_size,
     if (targets[n] > 0 && targets[n] < 1.0f) {
       counts[n] = 1;
       sum_nonrounded += targets[n];
-      sum += counts[n];
+      sum += 1;
     }
   }
   const float discount_ratio =
@@ -141,29 +143,26 @@ bool RebalanceHistogram(const float* targets, int max_symbol, int table_size,
   JXL_ENSURE(discount_ratio <= 1.0f);
   // Invariant for minimize_error_of_sum == true:
   // abs(sum - sum_nonrounded)
-  //   <= SmallestIncrement(max(targets[])) + max_symbol
+  //   <= SmallestIncrementNonzero(max(targets[])) + max_symbol
   for (int n = 0; n < max_symbol; ++n) {
     if (targets[n] >= 1.0f) {
       sum_nonrounded += targets[n];
-      counts[n] =
-          static_cast<ANSHistBin>(targets[n] * discount_ratio);  // truncate
-      if (counts[n] == 0) counts[n] = 1;
-      if (counts[n] == table_size) counts[n] = table_size - 1;
-      // Round the count to the closest nonzero multiple of SmallestIncrement
+      uint32_t count = targets[n] * discount_ratio + 0.5f;  // rounding
+      if (count == 0) count = 1;
+      if (count >= table_size) count = table_size - 1;
+      // Round the count to the closest nonzero multiple of SmallestIncrementNonzero
       // (when minimize_error_of_sum is false) or one of two closest so as to
       // keep the sum as close as possible to sum_nonrounded.
-      int inc = SmallestIncrement(counts[n], shift);
-      counts[n] -= counts[n] & (inc - 1);
-      // TODO(robryk): Should we rescale targets[n]?
-      const int target = minimize_error_of_sum
-                             ? (static_cast<int>(sum_nonrounded) - sum)
-                             : static_cast<int>(targets[n]);
-      if (counts[n] == 0 ||
-          (target >= counts[n] + inc / 2 && counts[n] + inc < table_size)) {
-        counts[n] += inc;
+      uint32_t inc = SmallestIncrementNonzero(count, shift);
+      count &= ~(inc - 1);
+      const float target = minimize_error_of_sum ? sum_nonrounded - sum
+                                                 : discount_ratio * targets[n];
+      if (target >= count + 0.5f * inc && count + inc < table_size) {
+        count += inc;
       }
-      sum += counts[n];
-      const int count_log = FloorLog2Nonzero(static_cast<uint32_t>(counts[n]));
+      sum += count;
+      counts[n] = count;
+      const int count_log = FloorLog2Nonzero(count);
       if (count_log > remainder_log) {
         remainder_pos = n;
         remainder_log = count_log;
@@ -173,7 +172,7 @@ bool RebalanceHistogram(const float* targets, int max_symbol, int table_size,
   JXL_ENSURE(remainder_pos != -1);
   // NOTE: This is the only place where counts could go negative. We could
   // detect that, return false and make ANSHistBin uint32_t.
-  counts[remainder_pos] -= sum - table_size;
+  counts[remainder_pos] -= static_cast<int>(sum) - static_cast<int>(table_size);
   *omit_pos = remainder_pos;
   return counts[remainder_pos] > 0;
 }

lib/jxl/jxl_test.cc
@@ -1081,7 +1081,7 @@ JXL_SLOW_TEST(JxlTest, RoundtripLossless8) {
   dparams.accepted_formats.push_back(t.ppf().frames[0].color.format);
 
   PackedPixelFile ppf_out;
-  EXPECT_EQ(Roundtrip(t.ppf(), cparams, dparams, pool.get(), &ppf_out), 223053);
+  EXPECT_EQ(Roundtrip(t.ppf(), cparams, dparams, pool.get(), &ppf_out), 223044);
   EXPECT_EQ(ComputeDistance2(t.ppf(), ppf_out), 0.0);
 }
 
@@ -1100,7 +1100,7 @@ JXL_SLOW_TEST(JxlTest, RoundtripLossless8ThunderGradient) {
   dparams.accepted_formats.push_back(t.ppf().frames[0].color.format);
 
   PackedPixelFile ppf_out;
-  EXPECT_EQ(Roundtrip(t.ppf(), cparams, dparams, pool.get(), &ppf_out), 261683);
+  EXPECT_EQ(Roundtrip(t.ppf(), cparams, dparams, pool.get(), &ppf_out), 261665);
   EXPECT_EQ(ComputeDistance2(t.ppf(), ppf_out), 0.0);
 }
 
@@ -1138,7 +1138,7 @@ JXL_SLOW_TEST(JxlTest, RoundtripLossless8Falcon) {
   dparams.accepted_formats.push_back(t.ppf().frames[0].color.format);
 
   PackedPixelFile ppf_out;
-  EXPECT_EQ(Roundtrip(t.ppf(), cparams, dparams, pool.get(), &ppf_out), 230762);
+  EXPECT_EQ(Roundtrip(t.ppf(), cparams, dparams, pool.get(), &ppf_out), 230753);
   EXPECT_EQ(ComputeDistance2(t.ppf(), ppf_out), 0.0);
 }
 
@@ -1158,7 +1158,7 @@ TEST(JxlTest, RoundtripLossless8Alpha) {
   dparams.accepted_formats.push_back(t.ppf().frames[0].color.format);
 
   PackedPixelFile ppf_out;
-  EXPECT_EQ(Roundtrip(t.ppf(), cparams, dparams, pool, &ppf_out), 251465);
+  EXPECT_EQ(Roundtrip(t.ppf(), cparams, dparams, pool, &ppf_out), 251456);
   EXPECT_EQ(ComputeDistance2(t.ppf(), ppf_out), 0.0);
   EXPECT_EQ(ppf_out.info.alpha_bits, 8);
   EXPECT_TRUE(test::SameAlpha(t.ppf(), ppf_out));
@@ -1472,7 +1472,7 @@ JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompression444) {
   const std::vector<uint8_t> orig =
       ReadTestData("jxl/flower/flower.png.im_q85_444.jpg");
   // JPEG size is 696,659 bytes.
-  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 568891u, 20);
+  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 568855u, 20);
 }
 
 JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompressionToPixels) {
@@ -1557,7 +1557,7 @@ JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompression420) {
   const std::vector<uint8_t> orig =
       ReadTestData("jxl/flower/flower.png.im_q85_420.jpg");
   // JPEG size is 546,797 bytes.
-  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 455510u, 20);
+  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 455481u, 20);
 }
 
 JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompressionLumaSubsample) {
@@ -1574,7 +1574,7 @@ JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompression444wh12) {
   const std::vector<uint8_t> orig =
       ReadTestData("jxl/flower/flower.png.im_q85_444_1x2.jpg");
   // JPEG size is 703,874 bytes.
-  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 569630u, 20);
+  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 569597u, 20);
 }
 
 JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompression422) {
@@ -1590,7 +1590,7 @@ JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompression440) {
   const std::vector<uint8_t> orig =
       ReadTestData("jxl/flower/flower.png.im_q85_440.jpg");
   // JPEG size is 603,623 bytes.
-  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 501101u, 20);
+  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 501077u, 20);
 }
 
 JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompressionAsymmetric) {
@@ -1608,7 +1608,7 @@ JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompression420Progr) {
   const std::vector<uint8_t> orig =
       ReadTestData("jxl/flower/flower.png.im_q85_420_progr.jpg");
   // JPEG size is 522,057 bytes.
-  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 455454u, 20);
+  EXPECT_NEAR(RoundtripJpeg(orig, pool.get()), 455420u, 20);
 }
 
 JXL_TRANSCODE_JPEG_TEST(JxlTest, RoundtripJpegRecompressionMetadata) {

commit 13665c8261b9e46ae669cfbde2c3533013954240

Fix RebalanceHistogram bug introduced in #4132

lib/jxl/enc_ans.cc
@@ -141,9 +141,9 @@ bool RebalanceHistogram(const float* targets, int max_symbol,
       (table_size - sum) / (table_size - sum_nonrounded);
   JXL_ENSURE(discount_ratio > 0);
   JXL_ENSURE(discount_ratio <= 1.0f);
-  // Invariant for minimize_error_of_sum == true:
-  // abs(sum - sum_nonrounded)
-  //   <= SmallestIncrementNonzero(max(targets[])) + max_symbol
+  // The case of minimize_error_of_sum == true should be able to rebalance
+  // any histogram, but it's hard to test this exhaustively
+  // since the number of possible inputs is too huge
   for (int n = 0; n < max_symbol; ++n) {
     if (targets[n] >= 1.0f) {
       sum_nonrounded += targets[n];
@@ -157,8 +157,15 @@ bool RebalanceHistogram(const float* targets, int max_symbol,
       count &= ~(inc - 1);
       const float target = minimize_error_of_sum ? sum_nonrounded - sum
                                                  : discount_ratio * targets[n];
-      if (target >= count + 0.5f * inc && count + inc < table_size) {
+      while (target >= count + 0.5f * inc && count + inc < table_size) {
         count += inc;
+        // `count` increased to the next power of 2 grows increment,
+        // but it cannot drop more digits
+        inc = SmallestIncrementNonzero(count, shift);
+      }
+      while (target < count - 0.5f * inc && count - inc > 0) {
+        count -= inc;
+        inc = SmallestIncrementNonzero(count, shift);
       }
       sum += count;
       counts[n] = count;