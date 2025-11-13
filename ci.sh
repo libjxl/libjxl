6910bf2fcc0f663318a4b186320be8a1d7dd6bf5
Evgenii Kliuchnikov <eustas@google.com>
2025-11-13 09:04:22 +0000

    Revive v0.8.x build

--- ci.sh
+++ ci.sh
@@ -513,7 +513,7 @@ strip_dead_code() {
 ### Externally visible commands
 
 cmd_debug() {
-  CMAKE_BUILD_TYPE="Debug"
+  CMAKE_BUILD_TYPE="DebugOpt"
   cmake_configure "$@"
   cmake_build_and_test
 }
