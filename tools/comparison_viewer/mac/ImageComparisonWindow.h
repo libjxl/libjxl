77a076b0c6805049d5ddec1ad7acf445f4261de1
Sami Boukortt <sboukortt@google.com>
2026-09-11 10:28:43 +0200

    Cocoa replacement for `compare_images` on macOS
    This is a bit less featureful than the Qt version but supports
    wide-gamut / HDR images and builds quickly with no non-system
    dependencies.

---
+++ tools/comparison_viewer/mac/ImageComparisonWindow.h
@@ -0,0 +1,14 @@
+// Copyright (c) the JPEG XL Project Authors. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#import <Cocoa/Cocoa.h>
+
+@interface ImageComparisonWindow : NSWindow
+
+- (instancetype)initComparisonBetween:(NSImage *)firstImage
+                                  and:(NSImage *)secondImage
+                        withReference:(NSImage *)referenceImage;
+
+@end
